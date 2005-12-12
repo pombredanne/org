d4c22dff3f62b313f1de7aadb20e29731a8fdaeb
bfreeman <bfreeman>
2005-12-12 22:29:05 +0000

    pictures to make the awards clearer

--- foundation/eclipseawards/individual_awards_guidelines.php
+++ foundation/eclipseawards/individual_awards_guidelines.php
@@ -42,9 +42,13 @@
 		  		<div align="center"><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119197"><img 
 		  		src="contributor-picture.gif" border="0"/></a></div></li>
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119198">Nominations for Top Committer Award</a>
-		  		- to be voted on by non-Committers.</li>
+		  		- to be voted on by non-Committers.<p>
+		  		<div align="center"><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119198"><img 
+		  		src="committer-picture.gif" border="0"/></a></div></li>
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119199">Nominations for Top Ambassador Award</a>
-		  		- to be voted on by everyone.</li>
+		  		- to be voted on by everyone.<p>
+		  		<div align="center"><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119199"><img 
+		  		src="ambassador-picture.gif" border="0"/></a></div></li>
 		</ul>
 		<p> Voting will commence February 10, 2006 and finishes March 10, 2006. 
 		Winners will be announced at <a href="http://www.eclipsecon.org/">EclipseCon 2006</a>. </p>
