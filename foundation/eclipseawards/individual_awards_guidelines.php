07f2b1e7fa076fe83d56622a937c79f538e9eca2
bfreeman <bfreeman>
2005-12-12 22:29:05 +0000

    pictures to make the awards clearer

--- foundation/eclipseawards/individual_awards_guidelines.php
+++ foundation/eclipseawards/individual_awards_guidelines.php
@@ -39,7 +39,7 @@
 		<ul class="midlist">
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119197">Nominations for Top Contributor Award</a> 
 		  		- to be voted on by Committers.<p>
-		  		<div align="center">a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119197"><img 
+		  		<div align="center"><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119197"><img 
 		  		src="contributor-picture.gif" border="0"/></a></div></li>
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=119198">Nominations for Top Committer Award</a>
 		  		- to be voted on by non-Committers.</li>
