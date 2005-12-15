7755ef3abe54c87518023a6d7f8ec278c6e5a38c
bfreeman <bfreeman>
2005-12-15 23:43:07 +0000

    test checkin

---
+++ councils/20051215PCMinutes.php
@@ -0,0 +1,24 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+$pageTitle 		= "December 2005 Planning Council Minutes";
+$pageKeywords	= "planning council minutes";
+$pageAuthor		= "Bjorn Freeman-Benson Dec 05";
+
+ob_start();
+?>
+    <div id="maincontent">
+	<div id="midcolumn">
+        <h1><?= $pageTitle ?></h1>
+
+MOREMORE
+       
+      </div>
+  </div>
+<?php
+	# Paste your HTML content between the EOHTML markers!	
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
