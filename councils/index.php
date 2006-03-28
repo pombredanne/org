eeed96f8409f294dc399169be2e90318516d3510
dosmith <dosmith>
2006-03-28 21:02:44 +0000

    *** empty log message ***

---
+++ councils/index.php
@@ -0,0 +1,8 @@
+<html>
+   <head>
+      <meta http-equiv="refresh" content="0; url=roadmap_v2_0/index.php" />
+   </head>
+   <body>
+      <a href="roadmap_v2_0/index.php">The current roadmap is version 2.0.</a>
+   </body>
+</html>
