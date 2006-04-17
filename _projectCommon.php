3aff6b319046eed310cfc42a67741e9a90d2f461
ngervais <ngervais>
2006-04-17 14:54:53 +0000

    links were not relative to root of site

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,9 +8,9 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank)
 	$Nav->addNavSeparator("About Us", 	"index.php");
-	$Nav->addCustomNav("Foundation", "foundation/", "_self", 1);	
-	$Nav->addCustomNav("Governance", "documents/", "_self", 1);
-	$Nav->addCustomNav("Legal Resources", "../legal/", "_self", 1);
-	$Nav->addCustomNav("Contact Us", "foundation/contact.php", "_self", 1);
+	$Nav->addCustomNav("Foundation", "/org/foundation/", "_self", 1);	
+	$Nav->addCustomNav("Governance", "/org/documents/", "_self", 1);
+	$Nav->addCustomNav("Legal Resources", "/legal/", "_self", 1);
+	$Nav->addCustomNav("Contact Us", "/org/foundation/contact.php", "_self", 1);
 
 ?>
