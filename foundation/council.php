594a78f5f82fc4ccc1ec92701ee9634b02446be6
dosmith <dosmith>
2006-04-21 14:20:54 +0000

    *** empty log message ***

--- foundation/council.php
+++ foundation/council.php
@@ -173,8 +173,11 @@
 		</table>
 		<br/>        	
 	<ul>
+		 <li><a href="../councils/20060319RCMinutes.pdf">Requirements Council Meeting, 
+          			March 19, 2006 (.pdf)</a><img src="../../images/new.gif" alt="new"></li>
+	
 	 	<li><a href="../councils/rcminutes050824.html">Requirements Council Meeting, 
-          			August 24, 2005</a><img src="../../images/new.gif" alt="new"></li>
+          			August 24, 2005</a></li>
         <li><a href="../councils/20041130RCMinutes.pdf">Requirements Council Meeting, November 30, 2004 (.pdf)</a></li>
         <li><a href="../councils/20040831RCMinutes.pdf">Requirements Council Meeting, 
             August 31, 2004 (.pdf)</a></li>
