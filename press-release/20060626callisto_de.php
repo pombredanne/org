96a0cde0c45b3f046b16f6b7d9a0b466ce98fd37
iskerrett <iskerrett>
2006-06-29 13:06:46 +0000

    *** empty log message ***

--- press-release/20060626callisto_de.php
+++ press-release/20060626callisto_de.php
@@ -24,39 +24,39 @@
 		<ul>
 		<li>Eclipse Community startet mit „Callisto“ erfolgreichen Synchron-Release von zehn Open-Source-Projekten </li>
 		</ul>
-		<p><strong>OTTAWA, KANADA – 26. Juni 2006 </strong>– Die Eclipse Foundation kündigte heute den 
-		gleichzeitigen Release von zehn Eclipse Open-Source-Projekten an. Dieses besondere Release-Ereignis 
-		namens Callisto ist einer der größten Multi-Projekt-Releases, den je eine Open-Source-Community unternommen 
-		hat. Die Eclipse Foundation bietet mit „Callisto“ die synchrone Einführung von zehn Einzelprojekten, die 
+		<p><strong>OTTAWA, KANADA – 26. Juni 2006 </strong>– Die Eclipse Foundation kündigte heute das 
+		gleichzeitigen Release von zehn Eclipse Open-Source-Projekten an. Dieses besondere Release-Ereignis namens 
+		Callisto ist eines der größten Multi-Projekt-Releases, das je eine Open-Source-Community unternommen hat. 
+		Die Eclipse Foundation bietet mit „Callisto“ die synchrone Einführung von zehn Einzelprojekten, die 
 		Anwendern, vor allem auch Einsteigern, die Einführung von offenen Werkzeugen erleichtern. Die 
-		Veröffentlichung von Callisto kennzeichnet das dritte Jahr in Folge, in dem Eclipse einen großen Release 
+		Veröffentlichung von Callisto kennzeichnet das dritte Jahr in Folge, in dem Eclipse ein großen Release 
 		innerhalb des vorgegebenen Zeitplans ausgeliefert hat. Die Projekte aus dem Callisto-Release stehen ab dem 
-		30. Juni 2006 zum Download unter www.eclipse.org/callisto bereit. </p>
+		30. Juni 2006 zum Download unter <a href="http://www.eclipse.org">www.eclipse.org/callisto</a> bereit.  </p>
 
-		<p>Callisto stellte ein voluminöses Vorhaben für die Eclipse-Community dar: mit 10 verschiedenen 
-		Projektteams, 260 Beteiligten und über 7 Millionen Codezeilen. 15 verschiedene unabhängige 
-		Software-Anbieter (ISVs) stellten Open-Source-Entwickler ab, um an den in Callisto enthaltenen Projekten 
-		zu arbeiten, und demonstrierten damit die von zahlreichen Anbietern unterstützte und globale Natur der 
-		Eclipse-Community. Diese Entwickler befanden sich in 12 verschiedenen Ländern weltweit: Kanada, USA, 
-		Finnland, Türkei, China, Frankreich, Russland, Tschechien, Indien, Schweiz, Deutschland und Österreich. </p>
+		<p>Callisto stellte ein voluminöses Vorhaben für die Eclipse-Community  mit 10 verschiedenen Projektteams, 
+		260 Beteiligten und über 7 Millionen Codezeilen dar. 15 verschiedene unabhängige Software-Anbieter (ISVs) 
+		stellen Open-Source-Entwickler ab, um an den in Callisto enthaltenen Projekten zu arbeiten und 
+		demonstrierten damit die von zahlreichen Anbietern unterstützte  globale Natur der Eclipse-Community. 
+		Diese Entwickler arbeiten  in 12 verschiedenen Ländern weltweit: Kanada, USA, Finnland, Türkei, China, 
+		Frankreich, Russland, Tschechien, Indien, Schweiz, Deutschland und Österreich.  </p>
 
 		<p>„Callisto verdeutlicht, dass das Open-Source-Entwicklungsmodell äußerst erfolgreich eine Plattform für 
 		die Softwareentwicklung bereitstellen kann“, sagt Mike Milinkovich, Geschäftsführer der Eclipse Foundation. 
-		„Als wir den Callisto-Release im letzten Jahr zum ersten Mal starteten, stellte dieser Versuch, so viele 
-		Projekte zu kombinieren, ein Risiko dar. Heute haben die Eclipse-Projekte bewiesen, dass eine verteilte C
-		ommunity, die gemeinsam an mehreren Projekten arbeitet und an der zahlreiche Anbieter beteiligt sind, sehr 
-		effektiv sein kann, um Software innerhalb eines vorhersehbaren Zeitplans auszuliefern.“</p>
+		„Als wir das Callisto-Release im letzten Jahr zum ersten Mal starteten, stellte dieser Versuch, so viele 
+		Projekte zu kombinieren, ein Risiko dar. Heute haben die Eclipse-Projekte bewiesen, dass eine verteilte 
+		Community, die gemeinsam an mehreren Projekten arbeitet und an der zahlreiche Anbieter beteiligt sind, sehr 
+		effektiv  Software innerhalb eines vorhersehbaren Zeitplans auszuliefern kann.“</p>
 
 		<p>Ein wichtiges Anliegen von Callisto ist es, Unternehmen wie etwa ISVs oder Enterprise-Entwicklern zu 
-		ermöglichen, Eclipse als Plattform für die Anwendungsentwicklung einfacher zu übernehmen. Die Benutzer 
+		ermöglichen, Eclipse als Plattform für die Anwendungsentwicklung einfacher zu adaptieren. Die Benutzer 
 		können von der modularen Architektur von Eclipse profitieren und für die Erstellung ihrer Anwendungen eine 
 		Vielzahl unterschiedlicher Eclipse-Projekte heranziehen. Callisto sorgt für ein einziges Release-Ereignis, 
 		das Versionskompatibilität und Zeitpläne synchronisiert. Das Synchron-Projekt ist jedoch nicht als 
 		eigenes, integriertes Werkzeugpaket zu verstehen, sondern stellt eine gleichzeitige Veröffentlichung 
-		verschiedener Projekte dar, die ihre eigene Roadmap beibehalten. Zu den ISVs, die planen Projekte aus 
-		dem Callisto-Release zu nutzen, gehören z. B. Actuate, AvantSoft, BEA, Borland, Business Objects, IBM, 
-		ILOG, Innoopract, Intalio, Intervoice, Klocwork, Lattix, LogicLibrary, Lombardi, Lynxworks, Oracle, QNX 
-		und Scapa Technologies.</p>
+		verschiedener Projekte dar, die ihre eigene Planung und Roadmap beibehalten, diese jedoch mit den anderen 
+		Projekten abgestimmt haben. Zu den ISVs, die planen, Projekte aus dem Callisto-Release zu nutzen, 
+		gehören z. B. Actuate, AvantSoft, BEA, Borland, Business Objects, IBM, ILOG, Innoopract, Intalio, 
+		Intervoice, Klocwork, Lattix, LogicLibrary, Lombardi, Lynxworks, Oracle, QNX und Scapa Technologies.</p>
 		
 		<p>Callisto beinhaltet den funktionalen Haupt-Release der folgenden Eclipse-Projekte:
 		<ul><br>
@@ -78,22 +78,21 @@
 			<ul><br>
 			<li><p><strong>Neue Standalone-OSGi-Laufzeitumgebung:</strong> Die Entwickler genießen jetzt die 
 			Flexibilität, die Eclipse OSGi-Laufzeitumgebung als Standalone-Framework nutzen zu können. Die 
-			OSGi-Laufzeitumgebung, Teil des Eclipse Equinox-Projekts, bildet die Grundlage der Eclipse Rich Client 
-			Platform. Als Standalone-Framework kann sie von den Entwicklern in den unterschiedlichsten 
-			Anwendungsarchitekturen eingesetzt werden, unter anderem für Embedded-, Client- und 
-			Server-Anwendungen.</p>
+			OSGi-Laufzeitumgebung, Teil des Eclipse Equinox-Projekts, bildet die Grundlage der Eclipse Rich 
+			Client Platform (RCP). Als Standalone-Framework wird sie von den Entwicklern in den unterschiedlichsten 
+			Anwendungsarchitekturen eingesetzt , unter anderem für Embedded-, Client- und Server-Anwendungen.</p>
 
  			<li><p><strong>Web Services Tooling:</strong>Angebot von Werkzeugen, die das Erstellen, die Weitergabe 
  			und das Profiling von Webdiensten vereinfachen. Neue Werkzeuge werden im Web Tools Project (WTP) sowie 
  			in der Test and Performance Tools Platform (TPTP) bereitgestellt.</p>
 
 			<li><p><strong>Neuer Plattform-Support für OSX auf Intel und Preview für Windows Vista:</strong>  
-			Eclipse-Entwickler können jetzt Anwendungen erstellen, die auf OSX auf Intel sowie unter Windows Vista 
+			Eclipse Entwickler können jetzt Anwendungen erstellen, die auf OSX auf Intel sowie unter Windows Vista 
 			laufen. </p>
 
- 			<li><p><strong>Release 1.0 von DTP und GMF:</strong>Die Data Tools Platform (DTP) stellt die 
- 			Frameworks für die Erstellung datenzentrierter Anwendungen bereit. Das Graphical Modeling Framework 
- 			macht es einfacher, Anwendungen zu erstellen, die grafische Editoren beinhalten.</p>
+ 			<li><p><strong>Release 1.0 von DTP und GMF:</strong>Die Data Tools Platform (DTP) stellt die Frameworks 
+ 			für die Erstellung datenzentrischer Anwendungen bereit. Das Graphical Modeling Framework erleichtet die 
+ 			Erstellung von , Anwendungen , die grafische Editoren beinhalten.</p>
 			</ul>
 			</p>
 
