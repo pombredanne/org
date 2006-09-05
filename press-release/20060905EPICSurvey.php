3201e8da8055dfc917d9f59e3a2dd9868734d271
dosmith <dosmith>
2006-09-05 17:57:38 +0000

    *** empty log message ***

--- press-release/20060905EPICSurvey.php
+++ press-release/20060905EPICSurvey.php
@@ -3,7 +3,7 @@
 
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Win an iPod - Eclipse Plugin Central (EPIC) Survey Now Live";
+	$pageTitle 		= "Help us make EPIC a better place and Win an iPod";
 	$pageKeywords	= "Eclipse Foundation, EPIC, community, bulletin";
 	$pageAuthor		= "Donald Smith";
 	
@@ -24,14 +24,16 @@
 		<table width="100%">
 			<tr>
 				<td width="50%" valign="top">
-     	<p>Provide feedback on Eclipse Plugin Central (EPIC) and have a chance to win a 30 gig iPod or one of 
-     	four Amazon $100 gift certificates!  We need your your feedback to help guide the ongoing
-     	enhancement of EPIC.  Complete the online survey between now and September 20th, 2006 and you will 
-     	be entered into the draw.  The survey will only take a few minutes of your time.
+     	<p>We would like to get your feedback on Eclipse Plugin Central (EPIC).  
+     	EPIC is a portal for all things in the Eclipse ecosystem and we are 
+     	looking to make it better.  Please take the time to comple the online 
+     	survey between now and September 20, 2006.   For everyone that completes 
+     	the survey we will enter you into a draw for a free iPod or one of four 
+     	Amazon $100 gift certificates.
      		</td>
 				<td width="40%">
 					
-					<a href="http://www.eclipseplugincentral.com/"><img src="http://eclipseplugincentral.com/themes/wvMSN/images/new_logo_1.jpg" border=0 alt="EPIC" title="EPIC"></a>					
+					<a href="http://www.surveymonkey.com/s.asp?u=938742505376"><img src="http://eclipseplugincentral.com/themes/wvMSN/images/new_logo_1.jpg" border=0 alt="EPIC" title="EPIC"></a>					
 				</td>
 			</tr>
 		</table>
