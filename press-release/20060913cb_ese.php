9cfcbc29c57efe0d74df3c5dd8cb79daec73cd52
siwai <siwai>
2006-09-13 14:33:19 +0000

    a few edits

--- press-release/20060913cb_ese.php
+++ press-release/20060913cb_ese.php
@@ -21,20 +21,36 @@
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
 		<h2>Community Bulletin</h2>
-      	<p>Participate at the Eclipse Summit in Esslingen, October 11-12th, 2006!   The Eclipse Summit Europe, presented by the Eclipse Foundation is a unique event here in Central Europe.   In addition to a variety of opportunities for networking with other members of the eclipse community, the program features tracks and events targetted for all parts of the community.</p>
+      	<p>Participate at the Eclipse Summit in Esslingen, October 11-12th, 2006!   The Eclipse Summit 
+      	Europe, presented by the Eclipse Foundation is a unique event in Central Europe.   
+      	In addition to opportunities for networking with other members of the eclipse 
+      	community, the program features tracks and events for all parts of the community.</p>
       	<p>
       	<ul>
-      		<li>developers and practitioners can find out the latest in a wide range of projects at eclipse.org through the Callisto Project Updates and New and Noteworthy track series.</li>   
-			<li>Foundation members and potential members can learn more about various aspects of the governance model of the Eclipse Foundation in the Foundation track </li>
-			<li>experienced developers can team up with other specialists in the Symposia, to learn, network and discuss in an open environment,  technical strategies in these specialized areas:  Test-Driven Embedded Development, Modelling, Rich Client Applications and Server Side Eclipse.</li>
+      		<li>developers and practitioners can find out the latest in 
+			 the Callisto Project Updates and New and Noteworthy track series.</li>   
+			<li>Foundation members and potential members can learn more about the governance model of 
+			the Eclipse Foundation in the Foundation track </li>
+			<li>Experienced developers can team up with other specialists in the Symposia, to learn, 
+			network and discuss in an open environment,  technical strategies in these specialized areas:  
+			Test-Driven Embedded Development, Modelling, Rich Client Applications and Server Side Eclipse.
+			</li>
 		</ul>
       	</p>
       	<p>This two day program also features events to extend your network within the Eclipse Community:
       		<ul>
-      			<li>Poster Reception - on Wednesday evening 17:00 - 19:00 we'll get together at the Neckar Forum for a project poster reception.   Meet the project committers in person and introduce yourself to your fellow ESE participants!</li> 
-				<li>BoFs - after the reception, there will be a limited number of BoF rooms available from 19:00-20:30 and again from 20:30 - 22:00.  If you'd like to organize a BoF, we'll have sign ups available starting on Tuesday evening.   </li>
-				<li>Eclipse Bistro - on Thursday afternoon, we'll close the program with the Eclipse Bistro. In addition to a fine sampling of German coffee and cakes, you'll have the opportunity to join various special interest groups in a bistro setting. You'll be able to continue the conversations you started at the Symposia, poster receptions or at the talks or create a new bistro table topic.   If you'd like to host a Bistro topic, please send a mail to <a href="mailto:susan_iwai@ch.ibm.com">susan_iwai@ch.ibm.com.</a></li> 
-				<li>Sponsor Network -   The Sponsor Network is an opportunity for you to meet with ecosystem members face to face and to acquaint yourself with their Eclipse based activities.  You can send member representatives a message now on <a href="http://www.eclipsecon.org/summiteurope2006/index.php?page=sponsornetwork/">http://www.eclipsecon.org/summiteurope2006/index.php?page=sponsornetwork/</a> .   We'll also have old-fashioned message boards for each Sponsor at the conference for those last minute arrangements.     Enjoy the networking!</li> 
+      			<li>Poster Reception -  Meet the project committers in person and introduce yourself to 
+      			your fellow ESE participants!</li> 
+				<li>BoFs - Sign up for Birds of a Feather sessions on site!</li>
+				<li>Eclipse Bistro - In addition to a fine sampling of German coffee and cakes, you'll 
+				have the opportunity to join various special interest groups in a bistro setting. You'll 
+				be able to continue the conversations you started at the Symposia, poster receptions or 
+				at the talks or create a new bistro table topic.</li> 
+				<li>Sponsor Network -   The Sponsor Network is an opportunity for you to meet with 
+				ecosystem members and to acquaint yourself with their Eclipse based 
+				activities.  You can send member representatives a message now on 
+				<a href="http://www.eclipsecon.org/summiteurope2006/index.php?page=sponsornetwork/">the Sponsor 
+				Network</a>.  Enjoy the networking!</li> 
       		</ul>
       	</p>
       	<p>Register now for the Eclipse Summit Europe on <a href="http://www.eclipsecon.org/summiteurope2006/">www.eclipsecon.org/summiteurope2006/ !</p>
