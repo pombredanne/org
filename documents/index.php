09116f274ecea0bccce632dd2d496e0eddec6951
ajacko <ajacko>
2007-01-24 08:03:01 +0000

    Changed board resolutions to html files.

--- documents/index.php
+++ documents/index.php
@@ -55,10 +55,10 @@
 				<li><a href="Eclipse_SpecOrgs_final.pdf">Standards and Specifications Policy:</a>
 					This document describes how the Eclipse Foundation and Eclipse projects 
 					interact with standards and specifications organizations.</li>
-				<li><a href="Eclipse_Project_Requirements.pdf">Project Requirements:</a> 
+				<li><a href="Eclipse_Project_Requirements.html">Project Requirements:</a> 
 					This document is a Board Resolution about the user interface and project data 
 					guidelines for Eclipse projects.</li>
-				<li><a href="Eclipse_Project_Usability.pdf">Project Usability:</a> 
+				<li><a href="Eclipse_Project_Usability.html">Project Usability:</a> 
 					This is a Board Resolution about requirements to improve project usability, 
 					initial user experience, and extensibility.</li>
 			</ul>
