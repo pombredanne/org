e5de9f371227132694b0802a0009a9ce18eee514
dosmith <dosmith>
2007-02-07 13:51:19 +0000

    *** empty log message ***

--- councils/roadmap_v3_0/index.php
+++ councils/roadmap_v3_0/index.php
@@ -1,11 +1,11 @@
-tart<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
 
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "2006 Eclipse Roadmap";
+	$pageTitle 		= "2007 Eclipse Roadmap";
 	$pageKeywords	= "roadmap, projects, strategy, documents, about, foundation";
-	$pageAuthor		= "Mike Milinkovich, Nov. 23, 2005";
+	$pageAuthor		= "Mike Milinkovich, Feb. 7, 2007";
 	
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
@@ -35,7 +35,7 @@ tart<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] 
 			<li><a href="#intro">Introduction</a></li>
 			<li><a href="#background">Background</a></li>
 			<li><a href="#strategic">Strategic Goals</a></li>
-			<li><a href="#progress">Progress in 2005</a></li>
+			<li><a href="#progress">Progress in 2006</a></li>
 			<li><a href="#future">Future Directions</a></li>
 			<li><a href="#process">The Roadmap Process</a></li>				
 		</ul>
@@ -150,7 +150,7 @@ tart<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] 
 	<p align = right><small><a href="#top">back to top</a></small></p>
 	
 	<hr>
-	<h3><a name="progress"></a>Progress in 2005</h3>
+	<h3><a name="progress"></a>Progress in 2006</h3>
 	<p>This is the second version of the Eclipse Roadmap document. It is important to recognize the many 
 	objectives which have been accomplished since the original version. The following is a list of accomplishments 
 	based on 2005&rsquo;s themes.</p>
