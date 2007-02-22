da51f844d57b341559535cbebcf6864bc740fda1
ajacko <ajacko>
2007-02-22 21:20:59 +0000

    Fixed typo on elections page.

--- elections/index.php
+++ elections/index.php
@@ -50,8 +50,7 @@
 	    <h3>$year Elections</h3>
 		<p>&nbsp;&nbsp;Information concerning the 2007 elections is contained within the links below:</p>
 		<ul>
-		<li>Voting has closed for the 2007 elections. The results will be announced soon; see these <a href="keydates.php">key dates.</a> 
-		<a href="../../vote2007/">elections page</a>.</li>
+		<li>Voting has closed for the 2007 elections. The results will be announced soon; see these <a href="keydates.php">key dates.</a> </li>
 		<li>The <a href="nominees.php">candidates</a> for the 2007 election.</li>
 		<li>The <a href="keydates.php">key dates</a> for the 2007 election.</li>
 		<li>The Eclipse Foundation <a href="election_process.php">election process.</a></li>
