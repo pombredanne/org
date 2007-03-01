b4cfb71df675fb7a380adb77a985f536474b797b
lgayowski <lgayowski>
2007-03-01 20:14:21 +0000

    updated to indicate past tense

--- foundation/eclipseawards/individual_awards_guidelines.php
+++ foundation/eclipseawards/individual_awards_guidelines.php
@@ -33,21 +33,21 @@
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
 		<h2>Guidelines</h2>
-		<p>The Individual Awards will determined by a vote of the community. One vote per person, per category.</p>
+		<p>The Individual Awards were determined by a vote of the community. One vote per person, per category.</p>
 		<p>Individuals may have been nominated by anyone in each category.   
 		<strong>Nominations closed on January 22, 2007. </strong> To see the Bugzilla entries for the individual nominations, visit the following links.</p>
 		<ul class="midlist">
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=168235">Nominations for Top Ambassador Award</a>
-		  		- to be voted on by everyone.</li>
+		  		- voted on by everyone.</li>
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=168237">Nominations for Top Contributor Award</a> 
-		  		- to be voted on by Committers.</li>
+		  		- voted on by Committers.</li>
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=168238">Nominations for Top Committer Award</a>
-		  		- to be voted on by everyone.</li>
+		  		- voted on by everyone.</li>
 
-		  <li>The <strong>Newcomer Evangelist Award</strong> will be awarded to the individual that answers the most questions posted
-		  on the <a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.newcomer">eclipse.newcomer</a> newsgroup. The period of calculation will be from February 15, 2006 to February 15, 2007. </li>
+		  <li>The <strong>Newcomer Evangelist Award</strong> is awarded to the individual that answered the most questions posted
+		  on the <a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.newcomer">eclipse.newcomer</a> newsgroup. The period of calculation is from February 15, 2006 to February 15, 2007. </li>
 		</ul>
-		<p> Voting will commence January 29, 2007 and finishes February 16, 2007. 
+		<p> Voting commenced January 29, 2007 and finished February 16, 2007. 
 		Winners will be announced at <a href="http://www.eclipsecon.org/">EclipseCon 2007</a>. </p>
 
 		<p><strong>Note:</strong> Eclipse is a large vibrant community of many well deserving individuals and technologies.  Therefore, to ensure diversity of award winners, we have decided
