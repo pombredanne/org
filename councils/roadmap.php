4bef73ccb6b81715805ef6d17d1782b1707cc766
dosmith <dosmith>
2007-03-18 13:00:57 +0000

    *** empty log message ***

--- councils/roadmap.php
+++ councils/roadmap.php
@@ -1,6 +1,6 @@
 <html>
    <head>
-      <meta http-equiv="refresh" content="0; url=roadmap_v2_0/index.php" />
+      <meta http-equiv="refresh" content="0; url=roadmap_v3_0/index.php" />
    </head>
    <body>
       <a href="roadmap_v3_0/index.php">The current roadmap is version 3.0.</a>
