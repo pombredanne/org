065c12163da30f022e9d4271a3c10e4d3b77fc0d
ngervais <ngervais>
2007-04-20 19:05:40 +0000

    *** empty log message ***

--- press-release/20070420cb_eclipseparty.php
+++ press-release/20070420cb_eclipseparty.php
@@ -22,11 +22,11 @@
 		<h1>$pageTitle</h1>
 		<h2>Community Bulletin</h2>
       	<p>
-        The Eclipse Foundation is hosting a party for the Eclipse community at JavaOne. If you will be attending the conference, this is your opportunity to connect with other Eclipse fans and to have some fun. The party will happen, Wednesday, May 9 from 7:00pm-10:00pm at Jillian’s (<a href="www.jilliansbilliards.com">www.jilliansbilliards.com</a>). This is just around the corner from the Moscone Center in the southwest corner of the Metreon.  If you are interested in attending, please send an e-mail to <a href="mailto:JavaOneParty@eclipse.org">JavaOneParty@eclipse.org</a> to be added to the guest list.  We look forward to seeing you there!<br/><br/>
+        The Eclipse Foundation is hosting a party for the Eclipse community at JavaOne. If you will be attending the conference, this is your opportunity to connect with other Eclipse fans and to have some fun. The party will happen, Wednesday, May 9 from 7:00pm-10:00pm at Jillian’s (<a href="http://www.jilliansbilliards.com">www.jilliansbilliards.com</a>). This is just around the corner from the Moscone Center in the southwest corner of the Metreon.  If you are interested in attending, please send an e-mail to <a href="mailto:JavaOneParty@eclipse.org">JavaOneParty@eclipse.org</a> to be added to the guest list.  We look forward to seeing you there!<br/><br/>
         Details:<br/><br/>
         Location: Jillian’s @ Metreon<br/>
 		Address: 101 Fourth Street, Suite 170 (<a href="http://maps.google.ca/maps/ms?ie=UTF8&hl=en&msa=0&msid=102535397083652435677.0000011200ac112cfedc1">see map</a>)<br/>
-		Date and Time: Wednesday, May 9, from 7:00 pm - 10:00 PM<br/>
+		Date and Time: Wednesday May 9, from 7:00 pm - 10:00 PM<br/>
 		RSVP: <a href="mailto:JavaOneParty@eclipse.org ">JavaOneParty@eclipse.org </a><br/>
         </p>
 	</div>
