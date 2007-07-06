8035d906d032195854bcbda42f1cc1087cdaa3e4
kmatthias <kmatthias>
2007-07-06 22:25:34 +0000

    Helps to actually include the file which does the DB connection.

--- foundation/directors.php
+++ foundation/directors.php
@@ -1,5 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
+require_once("/home/data/httpd/eclipse-php-classes/system/dbconnection_foundation_ro.class.php");
 	#*****************************************************************************
 	#
 	# directors.php
