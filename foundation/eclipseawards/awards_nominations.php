1dbf857f21d2e32d379ca574bee3774ff3c9ae03
lgayowski <lgayowski>
2007-12-05 16:20:31 +0000

    updated nominations info for 2008

--- foundation/eclipseawards/awards_nominations.php
+++ foundation/eclipseawards/awards_nominations.php
@@ -32,49 +32,53 @@
 
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
+		
 		<h2>Individual Awards</h2>
-		<p>Anyone may nominate an individual in each category. 
-		To nominate an individual visit the following Bugzilla entries. Nominations will close January 22, 2007. </p>
+		
+		<p>
+			Anyone may nominate an individual in each category.	 To nominate an individual visit the following Bugzilla entries.  
+			Nominations close January 25, 2008.
+		</p>
+		
 		<ul class="midlist">
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=168235">Nominations for Top Ambassador Award</a></li>
-		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=168237">Nominations for Top Contributor Award</a></li>
 		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=168238">Nominations for Top Committer Award</a></li>
+		  <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=168237">Nominations for Top Contributor Award</a></li>
 		</ul>
-		<p>NOTE: The <strong>Newcomer Evangelist Award</strong> is not open to nominations.  It will be calculated based on the
-		response to the eclipse.newcomer newsgroup.</p>
-		<h2>Technology Awards</h2>
-		<p>To submit a nomination for a technology awards send an e-mail by January 22, 2007 to <A 
-        href="mailto:awards@eclipse.org">awards@eclipse.org</A> with the following information:
-		<ul class="midlist">
-			<li>Category of award
-			<li>Name of person submitting award
-			<li>Contact information of person (telephone and e-mail)
-		</ul>	
-
-			<p class="midlist"> For <strong>Product Awards</strong>  (RCP and Eclipse developer tool) please answer the following: </p>
-			<ul class="midlist"><li>Name of product
-				<li>URL of product
-				<li>100 word description of product 
-				<li>Description of the usability features 
-				<li>Description of why and how the product uniquely solves a particular problem.   You might also make sure the problem is explained.
-				<li>Description of the completeness of the solution, including the technology, but also documentation, white papers, tutorials, and web site.  Everything you think is important for your customers to be successful with the product.
-				<li>OPTIONAL: if you are so inclined please feel free to send a 3-5 minute screen cam demo of your product.
-			</ul>
-			<p class="midlist">For the <strong>Best Deployment Award</strong> please answer the following:</p>
-			<ul class="midlist"><li>Name of organization
-				<li>Name of vendor submitting nomination
-				<li>Description of how Eclipse has been deployed in the enterprise (non-ISV).  Please include a description of the applications being built with Eclipse and the number of developers and end users.
-				<li>List of Eclipse projects being used with the deployment
-				<li>NOTE: This award is intended to recognized IT organizations that have deployed Eclipse internal to their enterprise.  Therefore, we have excluded ISVs from 
-				this catategoy.  ISVs are an important part of the Eclipse ecosystem but we feel they will be recognized in the other award categories. </li>
-			</ul>
-		</ul>	
+		
+		<p>
+			NOTE: The <strong>Newcomer Evangelist Award</strong> is not open to nominations.  It will be calculated based on the
+			response to the <a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.newcomer">eclipse.newcomer</a> newsgroup.
 		</p>
+		
+		<h2>Technology Awards</h2>
+		
+		<p>
+			To submit a nomination for a Technology Award, send an e-mail by January 25, 2008 to <a href="mailto:awards@eclipse.org">
+			awards@eclipse.org</a> with the information indicated below.
+
+			<ol class="midlist">
+				<li>Category of award</li>
+				<li>Name of person submitting award</li>
+				<li>Contact information of person (telephone and e-mail)</li>
+				<li>Name of product</li>
+				<li>URL of product</li>
+				<li>100 word description of product</li> 
+				<li>Description of the usability features</li> 
+				<li>Description of why and how the product uniquely solves a particular problem.  Ensure you also explain the problem.</li> 
+				<li>Description of the completeness of the solution, including the technology, but also documentation, white papers, tutorials, and
+				web site.  Include everything you think is important for your customers to be successful with the product.</li> 
+				<li>OPTIONAL: Feel free to send a 3-5 minute screen cam demo of your product</li> 
+			</ol>
+		</p>
+		
 		<h3>Previous Year Winners</h3>
-		<p>Eclipse is a large vibrant community of many well deserving individuals and technologies.  Therefore, to ensure diversity of award winners, we have decided
-		that previous year winners will not be eligible for a repeat award.   This does not mean that they are not deserving, it is just an acknowledgment that others are also 
-		deserving of recognition.</p><br><br>
-	
+		
+		<p>
+			Eclipse is a large, vibrant community of many well deserving individuals and technologies.  Therefore, to ensure diversity of
+			award winners, we have decided that winners from the previous year will not be eligible for a repeat award.  This does not mean
+			that they are not deserving, it is just an acknowledgment that others are also deserving of recognition.
+		</p>
 
 	</div>
 
