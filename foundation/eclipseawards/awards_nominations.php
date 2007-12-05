d080d7545a7b3af902f66f47568d046d671f0b35
lgayowski <lgayowski>
2007-12-05 16:29:44 +0000

    added links to navigation

--- foundation/eclipseawards/awards_nominations.php
+++ foundation/eclipseawards/awards_nominations.php
@@ -86,9 +86,11 @@
 		<div class="sideitem">
 			<h6>Related Links</h6>
 			<ul>
-				<li><a href="./technology_nominations.php">Current Nominations</a></li>
+				<li><a href="./index.php">Awards Categories</a></li>
 				<li><a href="./individual_awards_guidelines.php">Guidelines for Individual Awards</a></li>
 				<li><a href="./technology_awards_guidelines.php">Guidelines for Technology Awards</a></li>
+				<li><a href="./technology_nominations.php">Current Technology Nominations</a></li>
+				<li><a href="./judges08.php">Technology Awards Judges</a></li>
 			</ul>
 		</div>
 	</div>
