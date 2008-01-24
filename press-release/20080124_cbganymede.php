e5846dd93d231fe07de59883c21bbabea3482334
ngervais <ngervais>
2008-01-24 19:49:38 +0000

    *** empty log message ***

---
+++ press-release/20080124_cbganymede.php
@@ -0,0 +1,40 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include("_projectCommon.php");    # All on the same line to unclutter the user's desktop'
+
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Ganymede M4 packages now available for testing";
+	$pageKeywords	= "";
+	$pageAuthor		= "Nathan Gervais";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank)
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self");
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank");
+
+	# End: page-specific settings
+	#
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+	<style>
+		.paddedlist li {	padding-bottom:7px;	}
+	</style>
+<div id="maincontent">
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		<h2>Community Bulletin</h2>
+		
+		<p>For those that like to be using the latest and greatest, the Ganymede M4 packages are now available from the Eclipse Packaging Project (EPP) download site.</p>
+		
+ 		<p>This year EPP is going to produce packages for each Ganymede milestone and release candidate.  This will hopefully make it easier to test the packages before the final release and find cross-project issues.  Of course if you find issues with the packages, please report them in bugzilla.</p>
+ 
+	</div>
+</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
