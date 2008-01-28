4cf6230aeda3a0194f8d4ae78d3cc1f198249b24
lgayowski <lgayowski>
2008-01-28 20:40:09 +0000

    added thanks, but wait page

---
+++ foundation/eclipseawards/thankswait.php
@@ -0,0 +1,56 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# sample_3col.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-11-07
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Thank You";
+	$pageKeywords	= "eclipse, foundation, community, award, winners, 2008, judges";
+	$pageAuthor		= "Nathan Gervais";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the markers!	
+ob_start();
+?>		
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+
+		<p>
+			Thank you for casting your vote in the Eclipse Community Awards, Individuals category.  But you're not done yet!
+		</p>
+		<h1>Finalize Your Vote</h1>
+		<p>
+			We've now recorded your vote but you must confirm your choices via the
+			link in the email that is on its way to you.
+			Please check your email for the confirmation message and then click on the link
+			in that message to finalize your vote.
+		</p>
+		<center><img src="image/checkmark_2007.01.27.gif" border="0"></center>
+
+
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
