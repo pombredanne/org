863466971c2906d3d5dd192c57e9015424408627
lgayowski <lgayowski>
2008-02-15 15:18:44 +0000

    *** empty log message ***

--- press-release/20080219_RCPtraining.php
+++ press-release/20080219_RCPtraining.php
@@ -26,10 +26,11 @@
 		<h2>Community Bulletin</h2>
 	
 		<p>
-			The Eclipse Foundation is pleased to announce a series of <a href="http://www.regonline.com/CalendarNET/EventCalendar.aspx?CustomerId=240004&EventId=188879&view=Month">
+			Due to the success of the RCP training series Eclipse held last fall, the Eclipse Foundation is
+			pleased to announce that there will be a new series of <a href="http://www.regonline.com/CalendarNET/EventCalendar.aspx?CustomerId=240004&EventId=188879&view=Month">
 			RCP training classes</a> between May 19 and June 13.  In partnership with Eclipse member companies Ancit Consulting, Anyware
-			Technologies, AvantSoft, BREDEX, Industrial TSI, Innoopract, The RCP Company and WeigleWilczek, the RCP training series will
-			provide in-depth education for developers building Eclipse RCP-based applications.  Classes will be offered in
+			Technologies, AvantSoft, BREDEX, Industrial TSI, Innoopract, The RCP Company and Weigle Wilczek, Eclipse will be offering the
+			RCP training series to provide in-depth education for developers building Eclipse RCP-based applications.  Classes will be offered in
 			Antwerp, Bangalore, Boston, Braunschweig, Chicago, Copenhagen, Karlsruhe, Munich, Paris, Portland, San Jose, Sofia Antipolis,
 			Stockholm, Stuttgart, Vienna and Washington D.C.
 		</p>
@@ -37,7 +38,7 @@
 			Each RCP class will run for four days and will feature instructor-led classroom teaching and hands-on labs and tutorials. 					
 			The class will help developers understand the core RCP concepts and Eclipse plug-in architecture, and provide in-depth
 			insight into SWT, JFace and other RCP components.  There will also be a review of the new &amp; noteworthy features of
-			Ganymede, the Eclipse simultaneous release occuring in June.
+			Ganymede, the Eclipse simultaneous release occurring in June.
 		</p>
 		<p>
 			For more information, the locations, dates and registration can be found 
