4163b9f5baea06ab330062f02a2a1ab96c8c108d
lgayowski <lgayowski>
2008-03-03 21:33:02 +0000

    *** empty log message ***

--- foundation/eclipseawards/technology_nominations.php
+++ foundation/eclipseawards/technology_nominations.php
@@ -139,7 +139,7 @@ ob_start();
 			<h6>Related Links</h6>
 			<ul>
 				<li><a href="index.php">Awards Categories</a></li>			
-				<li><a href="technology_nominations.php">Current Technology Nominations</a></li>
+				<li><a href="technology_nominations.php">Technology Nominations</a></li>
 				<li><a href="individual_awards_guidelines.php">Guidelines for Individual Awards</a></li>
 				<li><a href="technology_awards_guidelines.php">Guidelines for Technology Awards</a></li>
 				<li><a href="awards_nominations.php">How to Make a Nomination</a></li>
