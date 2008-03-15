84e938528169e7cc4d6d79f71131bf9de82538fb
ajacko <ajacko>
2008-03-15 01:31:00 +0000

    Fixed typo.

--- foundation/index.php
+++ foundation/index.php
@@ -48,7 +48,7 @@
           	<li><A href="directions.php">Directions:</A>
 	          How to get to the Eclipse Foundation office in Ottawa.
           	</li>
-          	li><A href="directions_Portland.php">Directions:</A>
+          	<li><A href="directions_Portland.php">Directions:</A>
 	          How to get to the Eclipse Foundation office in Portland.
           	</li>
           </ul>
