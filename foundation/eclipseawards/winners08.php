19985bbdf1a4b6e48f5bcbc7b6e742058c86f1b6
lgayowski <lgayowski>
2008-03-25 14:46:38 +0000

    *** empty log message ***

--- foundation/eclipseawards/winners08.php
+++ foundation/eclipseawards/winners08.php
@@ -69,6 +69,7 @@ ob_start();
 			</ul>
 		</div>
 
+	<br><br>
 	<h2>Technology Award Winners</h2>
 		<p>Presented below is the list of award finalists for each of the technology awards catagories as determined
 			by this year's judging panels.</p>
