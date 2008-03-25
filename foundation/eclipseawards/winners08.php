965e0000f1b757ac28b8732ae9f20779a96db37f
lgayowski <lgayowski>
2008-03-25 14:50:18 +0000

    updated right menu

--- foundation/eclipseawards/winners08.php
+++ foundation/eclipseawards/winners08.php
@@ -124,9 +124,12 @@ ob_start();
 		<div class="sideitem">
 			<h6>Awards Navigation</h6>
 			<ul>
-				<li><a href="winners.php">Award Winners</a></li>
-				<li><a href="judges.php">Judges</a></li>
-				<li><a href="index.php">Explanation of Awards Categories</a></li>				
+				<li><a href="index.php">Awards Categories</a></li>
+				<li><a href="winners08.php">Winners &amp; Finalists</a></li>
+				<li><a href="technology_nominations.php">Technology Nominations</a></li>
+				<li><a href="individual_awards_guidelines.php">Guidelines for Individual Awards</a></li>
+				<li><a href="technology_awards_guidelines.php">Guidelines for Technology Awards</a></li>
+				<li><a href="awards_nominations.php">How to Make a Nomination</a></li>			
 			</ul>
 		</div>
 	</div>
