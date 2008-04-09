31ceda25414441bbf4b38ebe637b03dddde0c8ac
kmatthias <kmatthias>
2008-04-09 16:19:37 +0000

    Ed submitted his bio.

---
+++ foundation/boardbios/Ed_Merks.php
@@ -0,0 +1,8 @@
+Ed is a senior technical staff member at the IBM Toronto Software Lab.
+He leads the top-level <a
+href="http://www.eclipse.org/modeling/">Eclipse Modeling Project</a>
+along with Rich Gronback from Borland as well as the <a
+href="http://www.eclipse.org/modeling/emf/">Eclipse Modeling
+Framework</a> subproject. He holds a Ph.D. in Computing Science from
+Simon Fraser University and is an active <a
+href="http://www.ed-merks.blogspot.com/">blogger</a>.
