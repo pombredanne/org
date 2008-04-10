26b96678bf3f8575fc40bb8e7e92ec1c0b32c665
ajacko <ajacko>
2008-04-10 14:50:33 +0000

    Added Board bios.

---
+++ foundation/boardbios/Dave_Thomson.php
@@ -0,0 +1,11 @@
+Dave Thomson is a Distinguished Engineer at IBM's Ottawa lab (Canada). Dave 
+was one of the founding employees of Object Technology International (OTI), 
+a company IBM acquired in 1996. Prior to OTI's integration with IBM Dave 
+served as VP Development at OTI. Dave led the teams that delivered the 
+underlying IDE technology in many of IBM's award-winning products including: 
+VisualAge for Smalltalk, VisualAge for Java, and the Eclipse platform and was 
+a key contributor to the ANSI Smalltalk specification. Dave led the transition 
+of IBM's internal IDE platform development effort into the open source project 
+we now know as Eclipse, and is one of the visionaries behind the Jazz Project. 
+Dave is responsible for IBM's Eclipse and Jazz Development teams, and serves 
+as a Director on the board of the Eclipse Foundation. 
---
+++ foundation/boardbios/Mik_Kersten.php
@@ -0,0 +1,8 @@
+Mik Kersten is the President and CTO of Tasktop Technologies, lead of the 
+Eclipse Mylyn project, committer on the AspectJ project, and member of the 
+Eclipse Architecture Council. While a research scientist at Xerox PARC, Mik 
+implemented the first aspect-oriented programming IDE extensions. He then 
+created Mylyn and the Task-Focused Interface technology while completing 
+his PhD at the University of British Columbia. Mik's passion is building 
+Eclipse-based tools that offload our brains and make it easier to get 
+creative work done.
