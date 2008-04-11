42e15226ce161df722573f1fe15940d2175f641b
ajacko <ajacko>
2008-04-11 05:32:58 +0000

    Added board bio for Mark Coggins.

---
+++ foundation/boardbios/Mark_Coggins.php
@@ -0,0 +1,12 @@
+Mark Coggins is Senior Vice President of Engineering 
+at <a href="http://www.actuate.com">Actuate Corporation</a>. He
+has over 20 years of management experience in the software industry, 
+including a significant tenure in the software tools space. Among 
+previous positions held, Mark was VP of Engineering at 
+Interactive Development Environments, developers of the 
+"Software through Pictures" product line and the first company to 
+provide tool support for UML. He has an undergraduate degree
+in International Relations and a masters degree in Computer Science from
+Stanford University, and is the author of 
+<a href="http://www.immortalgame.com">four crime novels</a> set 
+in the Silicon Valley.
