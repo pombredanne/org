e570b5b7e6425b4e1f21fc2a9d5893aa591cf429
ajacko <ajacko>
2008-04-15 00:52:32 +0000

    Added Board bio.

---
+++ foundation/boardbios/Doug_Gaff.php
@@ -0,0 +1,4 @@
+Doug Gaff is the PMC Lead for the Device Software Development Platform (DSDP). Doug is also a Senior 
+Engineering Manager on Wind River's commercial Eclipse-based product, Wind River Workbench. He is responsible for 
+Eclipse open source contributions and adoption of Eclipse technology commercially. Doug has 13 years of embedded 
+software development experience and holds a Bachelors and Masters in Electrical Engineering.
