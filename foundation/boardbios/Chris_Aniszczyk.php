89f801d4145867e9b0fc4487136f053df5f4cdee
ajacko <ajacko>
2008-04-30 16:18:19 +0000

    Added board bio.

--- foundation/boardbios/Chris_Aniszczyk.php
+++ foundation/boardbios/Chris_Aniszczyk.php
@@ -1,3 +1,7 @@
-This is where a sample bio would be for Chris.  These will for now be stored in local files, but will
-be added to the portal.  That will allow board members to edit their own information as
-needed.
+Chris Aniszczyk is the technical lead for 
+the <a href="http://www.eclipse.org/pde/">Plug-in Development 
+Environment</a> (PDE) project at Eclipse. Chris also commits on 
+various other Eclipse projects and sits on the Eclipse Architecture 
+Council. Chris's passions are <a href="http://mea-bloga.blogspot.com">blogging</a>, 
+software advocacy, tooling and anything Eclipse. He's always available to 
+discuss open-source or Eclipse over a frosty beverage.
