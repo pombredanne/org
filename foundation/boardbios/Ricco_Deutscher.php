00c23c03979680ceb20011a8739aaa66885e1e13
ajacko <ajacko>
2008-05-14 18:15:21 +0000

    Added more board bios.

---
+++ foundation/boardbios/Ricco_Deutscher.php
@@ -0,0 +1,9 @@
+Dr. Ricco Deutscher is CTO and co-founder of SOPERA. Before joining 
+SOPERA, Ricco Deutscher was an architecture and technology expert at 
+McKinsey and in charge of international architecture projects. In this 
+capacity he developed the fields of SOA and Open Source at McKinsey. In 
+addition, Ricco Deutscher was also co-founder and president of flatfox 
+AG, a provider of email marketing technologies. He began his professional 
+career as a software architect at SAP AG in Walldorf. Dr. Ricco Deutscher 
+studied physics at the Universities of Leipzig and Moscow, and received a 
+Doctorate in Theoretical Physics from Hanover.
