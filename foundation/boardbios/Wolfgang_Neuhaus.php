73ada8bb34756cdde5033e0da9398bfb2c55c8ba
ajacko <ajacko>
2008-05-15 13:48:04 +0000

    Added more board bios.

---
+++ foundation/boardbios/Wolfgang_Neuhaus.php
@@ -0,0 +1,13 @@
+<p>Wolfgang Neuhaus is a co-founder and member of the board of itemis AG. Previously 
+he was Chief Information Officer (CIO) of Brockhaus AG and headed a team of 
+80 software developers and consultants.</p>
+
+<p>Wolfgang earned a degree in Computer Science from the University of 
+Dortmund in 1995. He has extensive practical experience in introducing and 
+implementing Model Driven Software Development, which is his core focus. 
+He is a well-known journal-published writer, an IT conference speaker, 
+and is responsible for the company's technical vision and for the 
+cooperation with research institutes.</p> 
+
+<p>He also is a member of the strategy committee for software intensive 
+embedded systems of BITKOM.</p> 
