0f51217ef1e94c934b7bf1bd30f44b9c6c89d13f
ajacko <ajacko>
2008-05-19 17:57:49 +0000

    Added more board bios.

---
+++ foundation/boardbios/Hans-Christian_Brockmann.php
@@ -0,0 +1,12 @@
+Chris Brockmann was educated in  Germany, the United Kingdom and the USA. He 
+studied business administration in Gšttingen (Germany) and 
+Winston Salem (USA) where he earned a MBA degree. He spent 10 years 
+with Baan Company's Solutions Center Automotiv, where he 
+built a strong background in production and logistics 
+planning. Later he was in charge of automotive retail solution 
+projects in Europe and Asia. He founded brox IT-Solutions GmbH in 
+1998 with the vision to help businesses make the most of the 
+massive amounts of available information. In 2008 brox and 
+partners proposed to the SMILA (previously EILF) project to become the 
+new open-architecture standard for handling unstructured information.  
+ 
