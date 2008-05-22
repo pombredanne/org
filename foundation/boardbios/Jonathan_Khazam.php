b6f3279e5a423065ad46e71cf0c605869de86acf
ajacko <ajacko>
2008-05-22 23:35:13 +0000

    Added Board bio.

---
+++ foundation/boardbios/Jonathan_Khazam.php
@@ -0,0 +1,10 @@
+Jon Khazam is Vice President & General Manager of the Manageability & Middleware Division 
+in Intel’s Software and Solutions Group.  He leads a worldwide organization responsible 
+for the development of value-add manageability-related software and middleware products, 
+including the Intel XML Software Suite. Jon became involved with Eclipse in 2003 while 
+he was leading the development of Intel software tools, including compilers, performance 
+analysis tools, performance libraries and binary translation. He has been a key contributor 
+to Intel’s software enabling efforts since the introduction of MMX Technology, which first 
+inspired his interest in software development tools. Jon has an MBA from the University of 
+California at Berkeley Haas School of Business and a bachelor's degree in electrical engineering 
+from Cornell University. He is also an independent board director at Integrated Silicon Solution, Inc.
