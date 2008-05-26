d2718ef38bc53c384f41d19f60fb345716fd87b3
ngervais <ngervais>
2008-05-26 14:25:25 +0000

    *** empty log message ***

--- press-release/20080426_EclipseSummitcfp_ger.php
+++ press-release/20080426_EclipseSummitcfp_ger.php
@@ -62,7 +62,7 @@ $html = <<<EOHTML
 
 EOHTML;
 
-
+	$html = mb_convert_encoding($html, "HTML-ENTITIES", "auto");
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
