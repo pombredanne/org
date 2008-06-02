9b40e325e6777aabdc3cba100f502689852680d5
ajacko <ajacko>
2008-06-02 23:03:41 +0000

    Added Board bio.

---
+++ foundation/boardbios/Robert_Day.php
@@ -0,0 +1,8 @@
+<p>Robert Day has spent his entire career involved with software development. For the last 20 years his focus 
+has been on embedded software; developing, selling and marketing many of the popular tools and 
+real-time operating systems. </p>
+<p>Robert has been heavily involved with the Eclipse Foundation since 2004, and has held the chair 
+of the Eclipse embedded workgroup since then. He has published many articles, given many conference papers, and 
+strongly promoted Eclipse into the embedded software market. His current position is VP of Marketing 
+at LynuxWorks, who now have standardized on the Eclipse platform for all their development tools.</p>
+ 
