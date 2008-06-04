eaf69b29c1c1eb5c47c1660d1a25f20c28e11313
ajacko <ajacko>
2008-06-04 00:20:58 +0000

    Fixed typo.

--- foundation/boardbios/Dennis_Leung.php
+++ foundation/boardbios/Dennis_Leung.php
@@ -1,5 +1,5 @@
 Dennis Leung is Vice-President of Development for Oracle Fusion Middleware, leading the development teams for the 
 award-winning Oracle TopLink product, Eclipse Dali, and EclipseLink projects. Dennis has been involved in 
 providing enterprise data solutions for over 14 years and also has a background in telecommunications. He 
-holds a bachelor’s degree in computer science from the University of British Columbia and a Master of 
+holds a bachelor's degree in computer science from the University of British Columbia and a Master of 
 Business Administration from Queens University.
