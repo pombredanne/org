7472d30319bf56549c505bfa21b8977c37027dd7
lgayowski <lgayowski>
2008-07-30 13:54:07 +0000

    added bio for Jochen

---
+++ foundation/boardbios/Jochen_Krause.php
@@ -0,0 +1,11 @@
+Jochen Krause is the project lead for the Eclipse <a href="<a href="http://www.eclipse.org/rap/">">Rich Ajax
+Platform (RAP)</a> project, and a member of the Board of Directors of the Eclipse Foundation.  He is CEO of
+<a href="http://www.innoopract.com">Innoopract</a>, a recognized leader in Eclipse distribution and web
+application development.  Jochen has had a leadership role in the Eclipse community since its inception in
+2002, and in addition to RAP development, co-leads the Eclipse Runtime PMC and participates in setting the
+directions for the next generation Eclipse Platform (e4). 
+<br><br>
+Jochen has focused Innoopract's product development on bringing Eclipse technology to enterprise customers,
+and at the same time has guided the company in making strategic technical contributions to Eclipse.  In
+addition to Jochen's activities, Innoopract staff are involved in many Eclipse projects such as Riena, Eclipse
+Packaging and G-Eclipse.
