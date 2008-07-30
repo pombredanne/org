ea79af61771e81f4bd08b4bed63d0f7edcda6a90
lgayowski <lgayowski>
2008-07-30 15:42:47 +0000

    added John Kellerman bio

---
+++ foundation/boardbios/John_Kellerman.php
@@ -0,0 +1,7 @@
+John joined IBM in 1984 with a computer science degree from Purdue University.  He’s since completed graduate
+degrees in Computer Engineering at North Carolina State and Business Administration at the University of North
+Carolina – Chapel Hill. He has spent the majority of his years at IBM in the development and management of
+application development tool products, including ISPF/PDF, VisualAge Smalltalk, Eclipse and Jazz.  John was
+a founding member of the Eclipse project. He is currently Product Manager for Jazz and Eclipse at IBM.
+Finally, John co-authored the award winning book, </i>Java Developer’s Guide to Eclipse</i>.  See
+<a href="http://www.jdg2e.com">www.jdg2e.com</a>.
