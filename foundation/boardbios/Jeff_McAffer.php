72d84ed946af611a8b7f203f2997921264326970
lgayowski <lgayowski>
2008-07-30 15:41:05 +0000

    added Jeff McAffer bio

---
+++ foundation/boardbios/Jeff_McAffer.php
@@ -0,0 +1,17 @@
+Jeff McAffer leads the Eclipse <a href="http://eclipse.org/equinox
+">Equinox OSGi</a>, <a href="http://eclipse.org/rcp">RCP</a> and <a 
+href="http://eclipse.org/orbit">Orbit</a> teams, and is the founder of <a 
+href="http://code9.com">Code 9</a>. He is one of the architects of the 
+Eclipse Platform and a co-author of <a href="http://eclipsercp.org"><i>The 
+Eclipse Rich Client Platform</i></a> (Addison-Wesley).  Jeff co-leads the Eclipse RT 
+PMC and is also a member of the 
+<a href="http://eclipse.org/eclipse/team-leaders.php">Eclipse Project 
+PMC</a>, the <a href="http://eclipse.org/tools/team-leaders.php">Tools 
+Project PMC</a>, the Eclipse Architecture Council and the Eclipse 
+Foundation Board of Directors.  Jeff is currently interested in all aspects 
+of Eclipse componentry, from designing, developing and building bundles, to deploying, 
+installing and ultimately running them.  Previous lives include being 
+a Senior Technical Staff Member at IBM, working in 
+distributed/parallel OO computing (Server Smalltalk, massively parallel 
+Smalltalk, etc.), as well as expert systems, meta-level architectures and a 
+PhD at the University of Tokyo.
