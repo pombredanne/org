18de17d6b5c9858be010eeb79df1295d67d8c7c5
dosmith <dosmith>
2008-07-30 18:09:56 +0000

    *** empty log message ***

--- foundation/boardbios/Ed_Merks.php
+++ foundation/boardbios/Ed_Merks.php
@@ -1,8 +1,14 @@
-Ed Merks is a senior technical staff member at the IBM Toronto Software Lab.
-He leads the top-level <a
-href="http://www.eclipse.org/modeling/">Eclipse Modeling Project</a>
-along with Rich Gronback from Borland as well as the <a
-href="http://www.eclipse.org/modeling/emf/">Eclipse Modeling
-Framework</a> subproject. He holds a Ph.D. in Computing Science from
-Simon Fraser University and is an active <a
-href="http://www.ed-merks.blogspot.com/">blogger</a>.
+Ed
+Merks leads the <a href="http://www.eclipse.org/emf">Eclipse Modeling Framework</a>
+project and co-leads the <a href="http://www.eclipse.org/modeling/">Eclipse
+Modeling</a> project along with Rich Gronback from Borland.&nbsp; He is a
+coauthor of the authoritative book “<a
+href="http://safari.informit.com/9780321331885">EMF: Eclipse Modeling Framework</a>”
+which is nearing completion of a second edition. He is interested in all aspects
+of Eclipse modeling and its applications.&nbsp; He is well recognized for his
+dedication to the Eclipse community, posting literally thousands of newsgroup
+answers each year.&nbsp; He is also an active <a
+href="http://www.ed-merks.blogspot.com/">blogger</a>.&nbsp; He spent 16 years
+at IBM, achieving the level of Senior Technical Staff Member after completing
+his Ph.D. at Simon Fraser Univeristy.&nbsp; His experience in modeling
+technology spans 25 years.
