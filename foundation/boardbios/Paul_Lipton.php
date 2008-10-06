2cd5c43a1b4a567c3432843721a90a815c18d37e
lgayowski <lgayowski>
2008-10-06 19:09:24 +0000

    *** empty log message ***

--- foundation/boardbios/Paul_Lipton.php
+++ foundation/boardbios/Paul_Lipton.php
@@ -8,12 +8,3 @@ highly sought-after author and speaker, and has shared his knowledge with apprec
 covering topics such as industry standards, SOA, open source, technical innovation, enterprise architecture,
 social computing, virtualization, Web services, management/security, governance, autonomic computing, Web 2.0
 and many other emerging technologies.
-
-
-Chris Aniszczyk is the technical lead for 
-the <a href="http://www.eclipse.org/pde/">Plug-in Development 
-Environment</a> (PDE) project at Eclipse. Chris also commits on 
-various other Eclipse projects and sits on the Eclipse Architecture 
-Council. Chris's passions are <a href="http://mea-bloga.blogspot.com">blogging</a>, 
-software advocacy, tooling and anything Eclipse. He's always available to 
-discuss open-source or Eclipse over a frosty beverage.
