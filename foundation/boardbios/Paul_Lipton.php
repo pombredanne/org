bee6b14d78a6a5f0362ed85902b42c9218a72072
lgayowski <lgayowski>
2008-10-06 19:07:27 +0000

    added Paul Lipton

---
+++ foundation/boardbios/Paul_Lipton.php
@@ -0,0 +1,19 @@
+Paul Lipton is an Advisor and Senior Architect in CA where he leads the CA Industry Standards and Open Source
+Program in the Office of the CTO. Paul has been an architect and developer of enterprise systems for over 20
+years. He has also participated in many standards organizations including OASIS and the W3C, and serves on the
+Board of Directors of the DMTF. Paul is a founding member of the CA Council for Technical Excellence, where he
+chairs the Emerging Technology Committee and also leads a project focused on leveraging Web 2.0 to improve
+research collaboration. He is also a Microsoft Most Valuable Professional and a Sun Java Champion. Paul is a
+highly sought-after author and speaker, and has shared his knowledge with appreciative audiences around the world
+covering topics such as industry standards, SOA, open source, technical innovation, enterprise architecture,
+social computing, virtualization, Web services, management/security, governance, autonomic computing, Web 2.0
+and many other emerging technologies.
+
+
+Chris Aniszczyk is the technical lead for 
+the <a href="http://www.eclipse.org/pde/">Plug-in Development 
+Environment</a> (PDE) project at Eclipse. Chris also commits on 
+various other Eclipse projects and sits on the Eclipse Architecture 
+Council. Chris's passions are <a href="http://mea-bloga.blogspot.com">blogging</a>, 
+software advocacy, tooling and anything Eclipse. He's always available to 
+discuss open-source or Eclipse over a frosty beverage.
