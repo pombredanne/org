ef50ade829219da2a6ffdc062faaef116cd8c262
ajacko <ajacko>
2008-10-10 21:54:49 +0000

    Updated Chris's bio.

--- foundation/boardbios/Chris_Aniszczyk.php
+++ foundation/boardbios/Chris_Aniszczyk.php
@@ -1,7 +1,6 @@
-Chris Aniszczyk is the technical lead for 
-the <a href="http://www.eclipse.org/pde/">Plug-in Development 
-Environment</a> (PDE) project at Eclipse. Chris also commits on 
-various other Eclipse projects and sits on the Eclipse Architecture 
-Council. Chris's passions are <a href="http://mea-bloga.blogspot.com">blogging</a>, 
-software advocacy, tooling and anything Eclipse. He's always available to 
-discuss open-source or Eclipse over a frosty beverage.
+Chris Aniszczyk is the technical lead for the Eclipse Plug-in Development Environment (PDE) 
+project and principal consultant at Code 9. He tends to be all over the place inside the 
+Eclipse community by committing on various Eclipse projects. He sits on the Eclipse 
+Architecture Council and on the Eclipse Technology PMC. His passions are blogging, 
+software advocacy, tooling, and anything Eclipse. He's always available to discuss 
+open source or Eclipse over a frosty beverage.
