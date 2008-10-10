665cb2e05dadc59ae3bb059e1c32c48b1adb1d0a
ajacko <ajacko>
2008-10-10 01:38:49 +0000

    Typo.

--- foundation/boardbios/Dino_Brusco.php
+++ foundation/boardbios/Dino_Brusco.php
@@ -3,7 +3,7 @@ is responsible for delivering software development tools and services to mobile
 application developers. Dino led the creation of Motorola’s first Eclipse-based 
 mobile application development platform.</p>
 
-<p>Dino’s previous senior management experience includes VP of Marketing, VP of R&D, 
+<p>Dino's previous senior management experience includes VP of Marketing, VP of R&D, 
 and VP of Mobile Software Developer Services for several high-tech software 
 companies. He has experience in a broad array of industries including enterprise, 
 mobile, communications infrastructure, and aerospace/defense.</p>
