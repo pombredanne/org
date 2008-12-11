88915b1870750a76fd538fa79e7bbfcf7a86a581
iskerrett <iskerrett>
2008-12-11 15:36:50 +0000

    *** empty log message ***

--- industry-workgroups/index.php
+++ industry-workgroups/index.php
@@ -1,5 +1,5 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-$pageTitle 		= "Eclipse Working Groups";
+$pageTitle 		= "Eclipse Industry Working Groups";
 $pageAuthor		= "";
 $pageKeywords	= "eclipse, working, groups";
 
