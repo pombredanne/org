401361f47df7a8a07b907af146d556f94a389588
bfreeman <bfreeman>
2008-12-12 19:40:33 +0000

    roadmap v4

--- councils/index.php
+++ councils/index.php
@@ -1,8 +1,8 @@
 <html>
    <head>
-      <meta http-equiv="refresh" content="0; url=roadmap_v3_0/index.php" />
+      <meta http-equiv="refresh" content="0; url=roadmap_v4_0/index.php" />
    </head>
    <body>
-      <a href="roadmap_v3_0/index.php">The current roadmap is version 3.0.</a>
+      <a href="roadmap_v4_0/index.php">The current roadmap is version 4.0.</a>
    </body>
 </html>
