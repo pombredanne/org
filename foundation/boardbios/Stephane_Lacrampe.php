b4412b09190b3731ba1e56286301860aefd4474f
lgayowski <lgayowski>
2009-01-19 16:45:12 +0000

    created Stephane bio

---
+++ foundation/boardbios/Stephane_Lacrampe.php
@@ -0,0 +1,9 @@
+Stephane Lacrampe is the CEO of Obeo. He's got a technical background in mathematics combined with IT. He
+started his career in the Telecom industry before switching to Internet technologies, working in a startup
+in London for two years. He also worked for very large insurance, banks and services companies back in
+France, where he learned about technical and methodological aspects of working with modeling technologies.
+<br><br>
+Stephane Lacrampe founded his own private company, Obeo, with two other associates in 2005, in order to
+provide innovative modeling and reverse engineering solutions based on Eclipse technologies. He is now in
+charge of growing the company and also focuses on growing the French Eclipse community.
+
