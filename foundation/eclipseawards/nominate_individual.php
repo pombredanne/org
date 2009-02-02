4ad5008520dbb7d39c3f959a8211d2c873de009e
lgayowski <lgayowski>
2009-02-02 19:40:37 +0000

    added blank line

--- foundation/eclipseawards/nominate_individual.php
+++ foundation/eclipseawards/nominate_individual.php
@@ -62,6 +62,7 @@
 		After nominations closed, voting commenced February 2, 2009 and will finish February 27, 2008 at 5:00 pm EST.  
 		</p>
 		
+		<br>
 		<h2>Past Winners</h2>
 		<p>
 			Eclipse is a large, vibrant community of many well-deserving individuals and technologies.
