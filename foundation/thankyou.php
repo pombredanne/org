e001055c0f08fe7010987ed2a78b4d4db5c23b6e
droy <droy>
2009-02-09 16:21:42 +0000

    Link to SLES, as requested by Novell email

--- foundation/thankyou.php
+++ foundation/thankyou.php
@@ -1,42 +1,18 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-
-	#*****************************************************************************
-	#
-	# sample_3col.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-11-07
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
-	
 	#
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "Thank You!";
 	$pageKeywords	= "thanks, donations, foundation";
 	$pageAuthor		= "Mike Milinkovich, Nov. 25, 2005";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 1);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 1);
-
-	# End: page-specific settings
-	#
 		
 	# Paste your HTML content between the EOHTML markers!	
 	$html = <<<EOHTML
-
 	<div id="midcolumn">
 	<h1>$pageTitle</h1>
      	<p>
         	The Eclipse Foundation would like to thank the following companies for 
         	their support of our website infrastructure:
       	</p>
-
 	<table width="100%" border="0" cellpadding="3" cellspacing=10>
   		<tr>
     		<td width="12%">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://www.eclipse.org/membership/scripts/get_image.php?id=848&size=small" width="120"></td>
@@ -77,12 +53,11 @@
   		<tr>
     		<td width="12%">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="/membership/scripts/get_image.php?id=716&size=small" width="120"></td>
     		<td width="88%"><br>
-      		Novell for donating Suse Enterprise distributions for all of our machines.<br>
+      		Novell for donating <a href="http://www.novell.com/linux/">SUSE Linux Enterprise</a> distributions for all of our machines.<br>
     		</td>
   		</tr>
 	</table>
 	</div>
-
 EOHTML;
 
 
