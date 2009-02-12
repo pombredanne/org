69b8bf95bdb62f2be14b1fb5706392ff48bc48a0
lgayowski <lgayowski>
2009-02-12 17:34:50 +0000

    added Karen's updates

--- press-release/20090213_eclipsist.php
+++ press-release/20090213_eclipsist.php
@@ -23,21 +23,26 @@
 		<h2>Community Bulletin</h2>
      	<p>
 			The Eclipse Foundation and Eclipse member eteration are pleased to announce
-			<a href="http://www.eclipsist.org/web/eclipsist2009/home">EclipsIST</a>, the third annual
-			Eclipse Technical and User Summit in Istanbul, Turkey.  EclipsIST takes place April 28-29, 2009. 
-			This event is for Eclipse users, adopters, developers and businesses.
-			It is an opportunity to participate in high quality technical tutorials and sessions delivered
-			by Eclipse experts.
+			<a href="http://www.eclipsist.org/web/eclipsist2009/home" target="blank">EclipsIST</a>, the third annual
+			Eclipse Technical and User Summit taking place in Istanbul, Turkey, April 28-29, 2009.
 		</p>
 		<p>
-			The <a href="http://www.eclipsist.org/web/eclipsist2009/program">conference program</a> is now available.
-			Attendance fees are waived but you must register in advance to attend.  Registration provides access to all presentations,
-			public receptions, lunches and the exhibit hall.
+			EclipsIST 2009 is a two-day event of fully loaded parallel speaker tracks followed by a
+			day of parallel power tutorials. With parallel sessions at any given time, attendees
+			can choose to attend the sessions that interest them.  All free of charge!   It is a
+			great opportunity to participate in high quality technical talks and tutorials
+			delivered by Eclipse experts from around the World.
 		</p>
- 		<p>
-	 		EclipsIST would like to thank our sponsors ebay, Eclipse Foundation, eclipse Magazin, eteration,
-	 		gitti gidiyo and Oracle who have made it possible to offer a no-cost event.
- 		</p>
+		<p>
+			<a href="http://www.eclipsist.org/web/eclipsist2009/registration" target="blank">Registration</a> is now open. Attendance fees are waived but you must register in advance
+			to attend.  Registration provides access to all presentations, public receptions,
+			lunches and the exhibit hall.
+		</p>
+		<p>
+			EclipsIST 2009 would like to thank our sponsors Eclipse Foundation, eclipse Magazin,
+			eteration, Eclipse Source, Gitti Gidiyor/eBay and Oracle who have made it possible to
+			offer a no-cost event.
+		</p>
 		  
 	</div>
 </div>
