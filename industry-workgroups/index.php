a9e7f0995953c10fa59e3a5fede88a048bbcee6c
ngervais <ngervais>
2009-02-18 16:56:11 +0000

    Updated to Nova Skin

--- industry-workgroups/index.php
+++ industry-workgroups/index.php
@@ -16,44 +16,38 @@ ob_start();
 	
 	
 </style>
-<div id="midcolumn">
-<h1><?=$pageTitle;?></h1>
-<div class="homeitem3col">
-<h3><a name="WorkingGroups"></a>Eclipse Industry Working Groups</h3>
-<div style="margin: 5px 15px 15px 15px; ">
-
-<table cellspacing=0 width="100%">
-	<tr>
-		<th width="50%">Group Name</th>
-		<th width="10%">Homepage</th>
-		<th width="10%">Mailing List</th>
-		<th width="10%">Newsgroup</th>
-	</tr>
-	<tr>
-		<td>Mobile Industry Working Group</td>
-		<td><a href="http://wiki.eclipse.org/EMIWG"><img src="http://www.eclipse.org/home/categories/images/wiki.gif"></a></td>
-		<td><a href="https://dev.eclipse.org/mailman/listinfo/mobile-iwg"><img src="http://www.eclipse.org/home/categories/images/mailinglist.gif"></a></td>
-		<td></td>
-	</tr>	
-
-</table>
-
+<div id="fullcolumn">
+	<div id="midcolumn">
+	<h1><?=$pageTitle;?></h1>
+		<div class="homeitem3col">
+			<h3><a name="WorkingGroups"></a>Eclipse Industry Working Groups</h3>
+			<div style="margin: 5px 15px 15px 15px; ">
+				<table cellspacing=0 width="100%">
+					<tr>
+						<th width="50%">Group Name</th>
+						<th width="10%">Homepage</th>
+						<th width="10%">Mailing List</th>
+						<th width="10%">Newsgroup</th>
+					</tr>
+					<tr>
+						<td>Mobile Industry Working Group</td>
+						<td><a href="http://wiki.eclipse.org/EMIWG"><img src="http://www.eclipse.org/home/categories/images/wiki.gif"></a></td>
+						<td><a href="https://dev.eclipse.org/mailman/listinfo/mobile-iwg"><img src="http://www.eclipse.org/home/categories/images/mailinglist.gif"></a></td>
+						<td></td>
+					</tr>	
+				</table>
+			</div>
+		</div>
+	</div>
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Working Group Process Document</h6>
+			<ul>
+				<li><a href="http://www.eclipse.org/org/industry-workgroups/industry_wg_process.php">Working Group Process Document</a></li>
+			</ul>
+		</div>
+	</div>
 </div>
-</div>
-
-
-
-</div>
-</div>
-
-<div class="homeitem3col">
-	<h3>Working Group Process Document</h3>
-	<ul>
-		<li><a href="http://www.eclipse.org/org/industry-workgroups/industry_wg_process.php">Working Group Process Document</a></li>
-	</ul>
-</div>
-</div>
-
 <? $html = ob_get_clean();
 	$App->generatePage($theme, $Menu, NULL, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
