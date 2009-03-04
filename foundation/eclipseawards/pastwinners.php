f2e705a73a421787086c7c2d0a44b7366b6bb0e6
lgayowski <lgayowski>
2009-03-04 22:39:50 +0000

    *** empty log message ***

---
+++ foundation/eclipseawards/pastwinners.php
@@ -0,0 +1,61 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# sample_list.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-11-07
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Past Winners of the Eclipse Awards";
+	$pageKeywords	= "eclipse, awards, individual, technology, nomination, eclipsecon";
+	$pageAuthor		= "Lynn Gayowski";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 1);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 1);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		
+		<ul class="midlist">
+			<li><a href="winners08.php">2008 Eclipse Community Awards Winners</li>
+			<li><a href="winners07.php">2007 Eclipse Community Awards Winners</li>
+			<li><a href="winners06.php">2006 Eclipse Community Awards Winners</li>
+		</ul>
+	</div>
+
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Related Links</h6>
+			<ul>
+				<li><a href="index.php">Eclipse Community Awards</a></li>
+				<li><a href="finalists09.php">2009 Finalists</a></li>
+				<li><a href="individual.php">Individual Nominations</a></li>
+				<li><a href="technology.php">Technology Nominations</a></li>
+				<li><a href="nominate_individual.php">Make an Individual Nomination</a></li>
+				<li><a href="nominate_technology.php">Make a Technology Nomination</a></li>
+			</ul>
+		</div>
+	</div>
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
