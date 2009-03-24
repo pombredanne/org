102199dde25aba8ee900134def81db017353e16f
lgayowski <lgayowski>
2009-03-24 02:36:46 +0000

    *** empty log message ***

--- foundation/eclipseawards/winners09.php
+++ foundation/eclipseawards/winners09.php
@@ -38,7 +38,7 @@ ob_start();
 		<div class="homeitem3col">
 			<h3>Top Ambassador</h3>
 			<ul>
-				<li class="winner">Nick Boldt</li>
+				<li class="winner">Nick Boldt (Winner)</li>
 				<li class="finalist">Mik Kersten</li>
 				<li class="finalist">Tom Schindl</li>
 			</ul>
@@ -49,14 +49,14 @@ ob_start();
 			<ul>
 				<li class="finalist">Martin Oberhuber</li>	
 				<li class="finalist">Jason Weathersby</li>
-				<li class="winner">Paul Webster</li>					
+				<li class="winner">Paul Webster (Winner)</li>					
 			</ul>
 		</div>
 
 		<div class="homeitem3col">
 			<h3>Top Contributor</h3>
 			<ul>
-				<li class="winner">Benjamin Cabe</li>
+				<li class="winner">Benjamin Cabe (Winner)</li>
 				<li class="finalist">Dave Carver</li>
 				<li class="finalist">Tom Schindl</li>
 			</ul>
@@ -67,7 +67,7 @@ ob_start();
 			<ul>
 				<li class="finalist">Nick Boldt</li>	
 				<li class="finalist">Walter Harley</li>
-				<li class="winner">Ed Merks</li>
+				<li class="winner">Ed Merks (Winner)</li>
 			</ul>
 		</div>			
 		<br />
@@ -83,14 +83,14 @@ ob_start();
 			<ul>
 				<li class="finalist"><a href="http://www.adobe.com/products/flex/" target="blank">Adobe Flex Builder 3</a></li>
 				<li class="finalist"><a href="http://www.guidancer.com" target="blank">Bredex GUIdancer</a></li>
-				<li class="winner"><a href="http://www.instantiations.com/windowbuilder/index.html?id=1" target="blank">Instantiations WindowBuilder Pro</a></li>
+				<li class="winner"><a href="http://www.instantiations.com/windowbuilder/index.html?id=1" target="blank">Instantiations WindowBuilder Pro</a> (Winner)</li>
 			</ul>
 		</div>	
 			
 		<div class="homeitem3col">
 			<h3>Best Open Source Eclipse-Based Developer Tool</h3>
 			<ul>
-				<li class="winner"><a href="http://www.acceleo.org" target="blank">Acceleo</a></li>
+				<li class="winner"><a href="http://www.acceleo.org" target="blank">Acceleo</a> (Winner)</li>
 				<li class="finalist"><a href="http://andrei.gmxhome.de/anyedit/index.html" target="blank">AnyEdit Tools</a></li>
 				<li class="finalist"><a href="http://openrules.com" target="blank">OpenRules</a></li>
 			</ul>
@@ -99,21 +99,21 @@ ob_start();
 		<h3>Best Commercial Equinox Application</h3>
 			<ul>
 				<li class="finalist"><a href="http://www.cas-pia.de" target="blank">CAS PIA</a></li>
-				<li class="winner"><a href="http://www.prosyst.com/products/osgi_ext_smart.html" target="blank">ProSyst mBedded Server Smart Home Extension</a></li>
+				<li class="winner"><a href="http://www.prosyst.com/products/osgi_ext_smart.html" target="blank">ProSyst mBedded Server Smart Home Extension</a> (Winner)</li>
 				<li class="finalist"><a href="http://eclipsesource.com/en/products/yoxos-enterprise/" target="blank">Yoxos Enterprise</a></li>
 			</ul>
 		</div>
 		<div class="homeitem3col">
 		<h3>Best Open Source Equinox Application</h3>
 			<ul>
-				<li class="winner"><a href="http://www.modulefusion.org" target="blank">ModuleFusion</a></li>
+				<li class="winner"><a href="http://www.modulefusion.org" target="blank">ModuleFusion</a> (Winner)</li>
 				<li class="finalist"><a href="http://vif.sourceforge.net/" target="blank">VIF - Virtual Forum</a></li>
 			</ul>
 		</div>
 		<div class="homeitem3col">
 			<h3>Best Commercial RCP Application</h3>
 			<ul>
-				<li class="winner"><a href="http://www.pluck-n-play.com" target="blank">Chord Scale Generator</a></li>
+				<li class="winner"><a href="http://www.pluck-n-play.com" target="blank">Chord Scale Generator</a> (Winner)</li>
 				<li class="finalist"><a href="http://www.anyware-tech.com/en/st/eclipse_platform/ecr_rcp_application.html" target="blank">ECR (Enregistrement Centralis&eacute; en R&eacute;seau - Centralized Network Recording)</a></li>
 				<li class="finalist"><a href="http://demos.netcetera.ch/nets" target="blank">NeTS - Network-wide Track Management System</a></li>
 			</ul>
@@ -121,7 +121,7 @@ ob_start();
 		<div class="homeitem3col">
 			<h3>Best Open Source RCP Application</h3>
 			<ul>
-				<li class="winner"><a href="http://directory.apache.org/studio" target="blank">Apache Directory Studio</a></li>
+				<li class="winner"><a href="http://directory.apache.org/studio" target="blank">Apache Directory Studio</a> (Winner)</li>
 				<li class="finalist"><a href="http://mae.cee.uiuc.edu/software_and_tools/maeviz.html" target="blank">MAEviz</a></li>
 			</ul>
 		</div>			
