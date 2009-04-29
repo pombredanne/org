f8257180be030d90f6c3f24d1dffa4d5e72ee3af
lgayowski <lgayowski>
2009-04-29 19:44:55 +0000

    added bio

--- foundation/boardbios/Michael_Bechauf.php
+++ foundation/boardbios/Michael_Bechauf.php
@@ -1,1 +1,5 @@
 <img src="boardbios/photos/Michael_Bechauf.jpg" align="left" style="padding-right: 5px;" width="120">
+Michael is responsible for SAP's standards strategy and participation in industry consortia. He is also
+responsible for the SAP Developer Network, a community of 1.4 million individual developers using the SAP
+NetWeaver platform. Michael has been a member of the JCP EC since 2001 and a Board member of the Eclipse
+Foundation since 2002.
