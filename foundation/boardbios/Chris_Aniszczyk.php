d92819d51c62984ee4b8d830959a274de4d10e6f
lgayowski <lgayowski>
2009-04-29 19:18:01 +0000

    *** empty log message ***

--- foundation/boardbios/Chris_Aniszczyk.php
+++ foundation/boardbios/Chris_Aniszczyk.php
@@ -1,7 +1,15 @@
-<img src="http://www.eclipse.org/org/foundation/boardbios/photos/Chris_Aniszczyk.jpg">
-Chris Aniszczyk is the technical lead for the Eclipse Plug-in Development Environment (PDE) 
-project and principal consultant at Code 9. He tends to be all over the place inside the 
-Eclipse community by committing on various Eclipse projects. He sits on the Eclipse 
-Architecture Council and on the Eclipse Technology PMC. His passions are blogging, 
-software advocacy, tooling, and anything Eclipse. He's always available to discuss 
-open source or Eclipse over a frosty beverage.
+<table>
+	<tr>
+		<td>
+			<img src="http://www.eclipse.org/org/foundation/boardbios/photos/Chris_Aniszczyk.jpg" width="120">
+		</td>
+		<td>
+			Chris Aniszczyk is the technical lead for the Eclipse Plug-in Development Environment (PDE) 
+			project and principal consultant at Code 9. He tends to be all over the place inside the 
+			Eclipse community by committing on various Eclipse projects. He sits on the Eclipse 
+			Architecture Council and on the Eclipse Technology PMC. His passions are blogging, 
+			software advocacy, tooling, and anything Eclipse. He's always available to discuss 
+			open source or Eclipse over a frosty beverage.
+		</td>
+	</tr>
+</table>
