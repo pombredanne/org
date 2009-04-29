e20fbc68427b89e160b56bcf5c119be4f63c7af8
lgayowski <lgayowski>
2009-04-29 19:28:45 +0000

    added Hans K.

---
+++ foundation/boardbios/Hans_Kamutzki.php
@@ -0,0 +1,7 @@
+<img src="boardbios/photos/Hans_Kamutzki.jpg" align="left" style="padding-right: 5px;" width="120">
+Hans Kamutzki is co-founder and Managing Director of MicroDoc, Munich. He studied Physics and Computer Science
+in Bonn and Munich, Germany and focussed on realtime image processing and process control in the early days of
+his professional work. For a "device and algorithm for the automatic detection of objects" he was co-awarded
+a German and European patent. After many years of development and project management practice in Smalltalk and
+Java, Hans now runs the Embedded Java operations in MicroDoc, working mainly in business development and
+legal/contractual matters.
