990f1372871d752d2490be06477be425ef7b43b3
lgayowski <lgayowski>
2009-04-29 19:25:51 +0000

    added Boris Bokowski

---
+++ foundation/boardbios/Boris_Bokowski.php
@@ -0,0 +1,8 @@
+<img src="boardbios/photos/Boris_Bokowski.jpg" align="left" style="padding-right: 5px;" width="120">
+Boris is the technical lead of the Eclipse Platform UI component and works for IBM in Ottawa, Canada. He is a
+member of the Eclipse Architecture Council, and an active committer on the e4 project. He is proud to have
+helped increase diversity at the Eclipse Platform (a little) by attracting several non-IBM contributors and
+helping them become committers. Before joining IBM in 2005, he wrote a commercial RCP app using EMF and GEF,
+co-founded and co-managed a Web 2.0 company in Germany, worked on the original Eclipse 1.0 at OTI in Ottawa,
+and earned a PhD in computer science at Freie Universitat Berlin, Germany. He is married and a geek dad of
+three kids. You can find him on IRC as "borisb6i". 
