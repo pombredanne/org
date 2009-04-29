e29dba2105d3b0d6d5bfd4875b6e29a7b0954316
lgayowski <lgayowski>
2009-04-29 19:36:29 +0000

    added Shawn Pearce

---
+++ foundation/boardbios/Shawn_Pearce.php
@@ -0,0 +1,12 @@
+<img src="boardbios/photos/Shawn_Pearce.jpg" align="left" style="padding-right: 5px;" width="120">
+<p>
+At Google, Shawn is committed to fostering the growth of the open source ecosystem by supporting existing
+programs such as Google Summer of Code, and the development of new communities, like the Android Open Source
+Project. Prior to joining Google, Shawn lead a development team at Bank of America building domain-specific
+plugins for the Eclipse platform.
+</p>
+
+<p>
+Outside of work, Shawn is an active contributor in the Git community, pushing the limits of distributed
+development tools.
+</p>
