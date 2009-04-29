04d93e3acd90e8f2204a1c65f8b494fab84577d3
lgayowski <lgayowski>
2009-04-29 19:36:52 +0000

    added photo

--- foundation/boardbios/Stephane_Lacrampe.php
+++ foundation/boardbios/Stephane_Lacrampe.php
@@ -1,3 +1,4 @@
+<img src="boardbios/photos/Stephane_Lacrampe.jpg" align="left" style="padding-right: 5px;" width="120">
 Stephane Lacrampe is the CEO of Obeo. He's got a technical background in mathematics combined with IT. He
 started his career in the Telecom industry before switching to Internet technologies, working in a startup
 in London for two years. He also worked for very large insurance, banks and services companies back in
