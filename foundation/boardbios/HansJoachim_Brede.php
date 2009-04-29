afed86e71573f0318fa58bce611fe4557b2293cd
lgayowski <lgayowski>
2009-04-29 19:31:13 +0000

    added Hans-J. Brede

---
+++ foundation/boardbios/HansJoachim_Brede.php
@@ -0,0 +1,5 @@
+<img src="boardbios/photos/HansJoachim_Brede.jpg" align="left" style="padding-right: 5px;" width="120">
+Hans-Joachim (Achim) Brede is the founder of BREDEX GmbH and is one of its managing directors. He has
+implemented and managed multiple, highly complex customer projects and is responsible for the concepts in the
+development of the automated test tool GUIdancer. Achim holds a Master in Computer Science and has worked with
+the University of Braunschweig and Digital Equipment as a technical consultant. 
