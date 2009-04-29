5fb79fb8f5f8a3f0f04fa866fe295377b6bbe2c7
lgayowski <lgayowski>
2009-04-29 19:38:33 +0000

    changed Hans-J. Brede file name

--- foundation/boardbios/Emma_McGrattan.php
+++
@@ -1,9 +0,0 @@
-Emma McGrattan is senior vice president of engineering at Ingres. She 
-is responsible for technology integration and the development of emerging 
-technologies. A leading authority in DBMS technologies, Emma has been 
-instrumental in the ongoing success of the Ingres product line. Emma 
-joined Ingres from CA where she held a similar position and was 
-responsible for the Ingres family of relational database management 
-products. She started her career with Ingres in 1992, and has held a 
-variety of senior development and management positions.  Born in Ireland, 
-Emma earned a Bachelor of Electronic Engineering from Dublin City University.
--- foundation/boardbios/Richard_Gronback.php
+++
@@ -1,7 +0,0 @@
-Richard Gronback is chief scientist for modeling products at Borland 
-Software Corporation, managing both open source and commercial product 
-development.  Richard co-leads the Modeling project Project Management 
-Committee (PMC), along with Ed Merks of IBM, and leads the Graphical 
-Modeling Framework (GMF) project.  Richard holds a BSE in Computer 
-Science & Engineering from the University of Connecticut, and was a 
-reactor operator in the U.S. Navy prior to his current career in software.
