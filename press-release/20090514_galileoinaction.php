50cb4c508cc6eeef58d6bbb6eecba4e3a74f3dd5
lgayowski <lgayowski>
2009-05-22 19:20:56 +0000

    *** empty log message ***

--- press-release/20090514_galileoinaction.php
+++ press-release/20090514_galileoinaction.php
@@ -25,24 +25,69 @@
 		
 		<h2>Community Bulletin</h2>
 		
-      	<p>The Eclipse community is preparing to release Galileo, the annual simultaneous release, at the end
-      	of June.  The release has now grown to 33 projects with over 18??? million lines of code, contributed
-      	by committers around the world.  With such a large global community, Eclipse wants to bring Galileo
-      	to you!</p>
-
- 		<p>On June 26, the Eclipse Foundation is presenting Galileo In Action - a day of virtual meetings
+      	<p>On June 26, the Eclipse Foundation is presenting Galileo In Action - a virtual conference
  		where you can interact with project leads involved in the release and see demos of the new
- 		features.  Galileo In Action is free to attend and registering gets you into all 5 sessions:
+ 		features. The annual simultaneous release has now grown to 33 projects with over 24 million
+ 		lines of code, contributed by committers around the world.  With such a large global community,
+ 		Eclipse wants to bring Galileo to you!</p>
+
+ 		<p>Galileo In Action is free to attend and registering gets you into all 5 sessions:
  		<ul>
- 			<li>10:00 am EST - <b>What is Galileo and Why do I Love it?</b>, presented by Doug Schaefer
- 			(C/C++ Development Tooling)	and Brian Fitzpatrick (Data Tools Platform)</li>
- 			<li>11:00 am EST - <b>Frameworks Showcase</b>, presented by Scott Lewis (Eclipse Communication
- 			Framework) and Pascal Rapicault (p2)</li>
- 			<li>12:00 pm EST - <b>Runtime Showcase</b>, presented by Tom Watson (Equinox) and Doug Clarke (EclipseLink)</li>
- 			<li>01:00 pm EST - <b>Tools Showcase</b>, presented by Roy Ganor & Michael Spector (PHP Development
- 			Tools)</li>
- 			<li>02:00 pm EST - <b>Galileo In Action</b>, presented by Jason Weathersby (Business Intelligence
- 			and Reporting Tools) and Mik Kersten (Mylyn)</li>
+ 			<li>10:00 am EST - <b>What is Galileo and Why do I Love it?</b>
+ 			<br>
+ 			Presented by Doug Schaefer (C/C++ Development Tooling) and Brian Fitzpatrick (Data Tools Platform)
+ 			<br><br>
+ 			Doug and Brian will give an overview of what Galileo is and explain why Eclipse projects want to 
+ 			participate in the release and what is required for them to do so.  They will describe the
+ 			improvements Galileo brings to the community.
+ 			</li>
+ 			
+ 			<li>11:00 am EST - <b>Frameworks Showcase</b>
+ 			<br>
+ 			Presented by Scott Lewis (Eclipse Communication Framework) and Pascal Rapicault (p2)
+ 			<br><br>
+ 			ECF is a framework for supporting the development of distributed Eclipse-based tools and
+ 			applications. It can be used to create other plugins, tools or full Eclipse RCP applications
+ 			that require asynchronous point-to-point or publish-and-subscribe messaging.
+			<br><br>
+ 			p2 provides a framework for provisioning Eclipse-based applications. It replaces Update Manager
+ 			as a mechanism for managing your Eclipse install, searching for updates and installing new
+ 			functionality. 
+ 			<br><br>
+ 			Scott and Pascal will give a short introduction to ECF and p2 and explain what these projects
+ 			have contributed to the Galileo release.
+ 			</li>
+ 			
+ 			<li>12:00 pm EST - <b>Runtime Showcase</b>
+ 			<br>
+ 			Presented by Tom Watson (Equinox) and Doug Clarke (EclipseLink)
+ 			<br><br>
+ 			From a code point of view, Equinox is an implementation of the OSGi R4 core framework
+ 			specification, a set of bundles that implement various optional OSGi services and other
+ 			infrastructure for running OSGi-based systems. The goal of the Equinox project is to be a
+ 			first class OSGi community and foster the vision of Eclipse as a landscape of bundles. As part
+ 			of this, it is responsible for developing and delivering the OSGi framework implementation
+ 			used for all of Eclipse.
+ 			<br><br>
+ 			EclipseLink, or the Eclipse Persistence Services Project, delivers a comprehensive open-source
+ 			Java persistence solution. It has a set of persistence services enabling developers to
+ 			efficiently develop applications that access data in a variety of data sources and formats.
+			<br><br>
+ 			Join Tom and Doug for a review of these runtime projects and the new features they bring
+ 			to Galileo.
+ 			</li>
+ 			
+ 			<li>01:00 pm EST - <b>Tools Showcase</b>
+ 			<br>
+ 			Presented by Roy Ganor & Michael Spector (PHP Development Tools)
+ 			<br><br>
+ 			</li>
+ 			
+ 			<li>02:00 pm EST - <b>Galileo In Action</b>
+ 			<br>
+ 			Presented by Jason Weathersby (Business Intelligence and Reporting Tools) and Mik Kersten (Mylyn)
+ 			<br><br>
+ 			</li>
  		</ul>
  		</p>
  		
