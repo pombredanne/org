e5e883a8cb84aefb3cef68c099a91748cab9fa69
lgayowski <lgayowski>
2009-08-13 20:55:51 +0000

    *** empty log message ***

---
+++ foundation/boardbios/Todd_E_Williams.php
@@ -0,0 +1,11 @@
+<img src="boardbios/photos/Todd_Williams.jpg" align="left" style="padding-right: 5px;" width="120">
+<p>Todd Williams is Vice President of Technology and co-founder of Genuitec and has been involved with Eclipse since its public 
+launch in 2001. He is a recognized speaker and author on Eclipse technologies and his publications include a 2002 article 
+that  became the genesis of the Eclipse rich-client platform. Todd has over 20 years of experience in development of computing 
+infrastructures, large scale distributed software architectures, and optimization
+of development processes, techniques and tools.</p>
+
+<p>Prior to Genuitec, Todd served as Enterprise Architect for Viewlocity's Global Supply Chain Event Management Suite, 
+Senior Consulting Architect at Objectspace, and Parallel Systems Consultant for Mobil's Advanced Research Center.  He 
+holds a B.S. in Computer Science from Texas A&M University and an M.S. in Computer Science from the University of Texas at Dallas.</p>
+
