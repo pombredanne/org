01285e78543c445cedf4e6ab658f4c661618ba27
mmilinkovich <mmilinkovich>
2009-09-08 20:51:44 +0000

    Added link to the June 2009 Board of Directors meeting minutes

--- foundation/minutes.php
+++ foundation/minutes.php
@@ -60,8 +60,11 @@
 			<h3>Board of Directors Meetings</h3>
 			<ul>
 				<li>Abridged Minutes 
-          			- <a href="boardminutes/2009_03_23_Minutes.php">March 23, 
+          			- <a href="boardminutes/2009_06_17-18_Minutes.php">June 17-18, 
           			2009 (html)</a><img src="../../images/new.gif" alt="new"></li>
+				<li>Abridged Minutes 
+          			- <a href="boardminutes/2009_03_23_Minutes.php">March 23, 
+          			2009 (html)</a></li>
 				<li>Abridged Minutes 
           			- <a href="boardminutes/2009_02_18_Minutes.php">February 18, 
           			2009 (html)</a></li>
