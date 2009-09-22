c9019f5fe16ca586b65bac430f1d641fc8ec1a22
lgayowski <lgayowski>
2009-09-22 20:16:19 +0000

    *** empty log message ***

--- press-release/index.php
+++ press-release/index.php
@@ -36,6 +36,7 @@ ob_start();Tha
 		<h2>Current Press Releases</h2>
 		<hr size="1" />
 		<ul>
+		<li><a href="20090922_ese1month.php">Eclipse Summit Europe Starts in One Month - Register Today</a> - September 22, 2009</li>
 		<li><a href="20090908_training.php">Global Eclipse Training Series</a> - September 8, 2009</li>
 		<li><a href="20090903_eseprogram.php">Keynotes and Technical Program Announced for Eclipse Summit Europe 2009</a> - September 3, 2009</li>
 		<li><a href="20090806_e4cb.php">Join the e4 Feedback Contest & Webinars</a> - August 6, 2009</li>
