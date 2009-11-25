dc49f676fbfeaa353d7b8f4c197eea52d2888b7c
lgayowski <lgayowski>
2009-11-25 18:05:47 +0000

    *** empty log message ***

--- foundation/eclipseawards/pastwinners.php
+++ foundation/eclipseawards/pastwinners.php
@@ -34,10 +34,11 @@
 		<h1>$pageTitle</h1>
 		
 		<p>
-			Congratulations to all winners and finalists of the Eclipse Community Awards.
+			Congratulations to all past winners of the Eclipse Community Awards.
 		</p>
 		
 		<ul class="midlist">
+			<li><a href="winners09.php">2008 Eclipse Community Awards Winners</li>
 			<li><a href="winners08.php">2008 Eclipse Community Awards Winners</li>
 			<li><a href="winners07.php">2007 Eclipse Community Awards Winners</li>
 			<li><a href="winners06.php">2006 Eclipse Community Awards Winners</li>
@@ -49,13 +50,12 @@
 			<h6>Related Links</h6>
 			<ul>
 				<li><a href="index.php">Eclipse Community Awards</a></li>
-				<li><a href="winners09.php">2009 Winners</a></li>
-				<li><a href="judges09.php">2009 Judges</a></li>
+				<li><a href="nominate_individual.php">Make an Individual Nomination</a></li>
+				<li><a href="nominate_technology.php">Make a Technology Nomination</a></li>
 				<li><a href="individual.php">Individual Nominations</a></li>
 				<li><a href="technology.php">Technology Nominations</a></li>
 				<li><a href="pastwinners.php">Past Winners</a></li>
-				<li><a href="nominate_individual.php">Make an Individual Nomination</a></li>
-				<li><a href="nominate_technology.php">Make a Technology Nomination</a></li>
+
 			</ul>
 		</div>
 	</div>
