9c14e5209867cee31387435517684115af6b7f56
lgayowski <lgayowski>
2009-11-25 19:39:18 +0000

    *** empty log message ***

--- foundation/eclipseawards/individual.php
+++ foundation/eclipseawards/individual.php
@@ -51,323 +51,30 @@ function switchMenu(obj) {
 		<h1><?= $pageTitle ?></h1>
 		
 		<p>
-			The following is a list of nominations submitted for the Individual Awards.  Nominations closed on January 30, 2009.
+			The following is a list of nominations submitted for the Individual Awards.  Nominations close on January 29, 2009.
 		</p>
-		
-<h3>Top Ambassador</h3>
-
-		<p><a href="javascript:switchMenu('NickBoldt');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Nick Boldt
-		</p>
-		<div id="NickBoldt" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I'd like to nominate Nick Boldt.  Look at all those fun contests he's initiated
-			this year, e.g.,<br>
-			<a href="http://divby0.blogspot.com/2008/05/ganymede-poster-contest.html">http://divby0.blogspot.com/2008/05/ganymede-poster-contest.html</a><br>
-			<a href="http://divby0.blogspot.com/2008/11/eclipsecon-2009-t-shirt-design-contest.html">http://divby0.blogspot.com/2008/11/eclipsecon-2009-t-shirt-design-contest.html</a><br>
-			<a href="http://divby0.blogspot.com/2008/11/eclipse-photoshop-challenge-better-than_25.html">http://divby0.blogspot.com/2008/11/eclipse-photoshop-challenge-better-than_25.html</a>
-			<br><br>
-			You'll find him on IRC answering questions.  You'll find him blogging, often in
-			the form of poetry set to music.  You'll find him on newsgroups like newcomers,
-			platform, PDT, and so on answering questions.  And he helps many groups set up
-			their builds, especially those new to Eclipse who need a helping hand.  
-			<br><br>
-			Nick reflects the spirit of Eclipse with his attitude and his actions.  That's
-			what ambassadors do..."
-			</li>
-			<li>
-			"+1 for Nick. It is amazing how many (good ;-) things he manages to do,
-			including being responsible to make sure that multiple builds are working and
-			all the fun stuff Ed mentioned."
-			</li>
-			<li>
-			"+1 for Nick Boldt, definitely! His contests are pure fun and he has done great
-			work this year setting up better builds for PDT, blogging, helping people on
-			newsgroups and IRC, and spreading the word about kiva.org :-) "
-			</li>
-		</ul>
-		
-		</p>
-		</div>	
-		
-		<p><a href="javascript:switchMenu('PaulFremantle');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Paul Fremantle
-		</p>
-		<div id="PaulFremantle" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"Paul Fremantle, WSO2 co-founder and CTO , has served as an ambassador for
-			Eclipse and its role in contributing to a flexible, heterogeneous SOA, both
-			through development of WSO2 products that support Eclipse and continuing
-			education of the developer community via conferences, webinars, blogs and the
-			WSO2 forum. However, his most important initiative has been around the use of
-			Eclipse Equinox to fundamentally redefine the structure of SOA
-			middleware—extending beyond the componentization of individual products to
-			create a fully componentized SOA middleware platform. 
-			<br><br>
-			With WSO2 Carbon, Paul has demonstrated the power of Equinox to create an SOA
-			fabric in which developers can mix and match components of different products
-			to assemble highly customized products for their individual needs. Since late
-			last year, Paul has not only evangelized WSO2’s specific approach, but also
-			the architectural principals of creating a fully componentized SOA platform
-			enabled by Eclipse Equinox."
-			</li>
-		</ul>
-		</p>
-		</div>
-		
-		<p><a href="javascript:switchMenu('MikKersten');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Mik Kersten
-		</p>
-		<div id="MikKersten" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I would like to nominate Mik Kersten for the Top Ambassador award. 
-			<br><br>
-			Mik has been evangelizing Eclipse in highly visible venues that reach beyond
-			the current Eclipse ecosystem. He presented a widely attended talk at JavaOne
-			that earned him a JavaOne Rock Star Hall of Fame award. Mik is the first member
-			of the Eclipse ecosystem to be recognized with this award. He has also been
-			promoting Eclipse and RCP at numerous other conferences, including a recent
-			keynote talk at W-JAX, talks at SpringOne conferences, and some of the most
-			highly rated talks on the Eclipse conference circuit. 
-			<br><br>
-			Mik's Eclipse Mylyn 2.2 and 3.0 videos have been viewed over 20,000 times
-			(combined), making them the most highly viewed Eclipse webcasts on Eclipse
-			Live. As CEO of Tasktop Technologies, Mik is demonstrating the capabilities of
-			the Eclipse platform by basing all of his company's innovative products and
-			services on Eclipse and RCP."
-			</li>
-		</ul>
-		</p>
-		</div>
-		
-		<p><a href="javascript:switchMenu('TomSchindla');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Tom Schindl
-		</p>
-		<div id="TomSchindla" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I'd like to nominate Tom Schindl. 
-			<br><br>
-			He is helping everywhere in the newsgroups, does an excellent promotional work
-			at conferences/blogs/papers and is very active in bringing forward new and
-			established Eclipse technologies."
-			</li>
-		</ul>
-		</p>
-		</div>
-		
-		<p><a href="javascript:switchMenu('JamesSugrue');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			James Sugrue
-		</p>
-		<div id="JamesSugrue" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I would like to nominate James Sugrue. James has worked tirelessly podcasting
-			interviews with Eclipse community members, blogging and writing articles on
-			Eclipse.
-			<br><br>
-			He's also managed to revive EclipseZone which is great. I particularly enjoyed
-			reading the EMF team interview
-			(<a href="http://eclipse.dzone.com/articles/eclipse-modelling-framework-qa">
-			http://eclipse.dzone.com/articles/eclipse-modelling-framework-qa</a>). I can only
-			hope he does more of those types of things&nbsp;;)&nbsp;"	
-			</li>
-		</ul>
-		</p>
-		</div>
-
-		<p>
-		To read more about the candidates, click on the plus sign beside each name and
-		<a href="http://eclipse.dzone.com/articles/meet-years-top-ambassador" target="blank">see the DZone interview</a> with the Top Ambassador nominees.
-		</p>		
 		
 		<h3>Top Committer</h3>
 
 		<p><a href="javascript:switchMenu('MartinOberhuber');" title="Description">
 		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Martin Oberhuber
+			Name1
 		</p>
-		<div id="MartinOberhuber" class="switchcontent">
+		<div id="Name1" class="switchcontent">
 		<p>
 		<ul>
 			<li>
-			"I would like to also nominate Martin Oberhuber. He's done a great job leading
-			the TM project at Eclipse (<a href="http://www.eclipse.org/dsdp/tm/">http://www.eclipse.org/dsdp/tm/</a>) but more
-			importantly, he's done something I thought impossible, revive the Eclipse
-			Architecture Council (EAC). Because of Martin's leadership, the EAC has become
-			a place where committers can ask questions easily
-			(<a href="http://dev.eclipse.org/mhonarc/lists/eclipse.org-committers/msg00598.html">http://dev.eclipse.org/mhonarc/lists/eclipse.org-committers/msg00598.html</a>),
-			look at open issues (<a href="http://wiki.eclipse.org/Architecture_Council/Open_Issues">http://wiki.eclipse.org/Architecture_Council/Open_Issues</a>),
-			seek mentorship and many other things.
-			<br><br>
-			Thank you Martin."	
+			Comment 1	
 			</li>
 			<li>
-			"Martin gets my vote for bringing the AC back from the dead and moving towards a
-			body that can provide support and services to all committers."
-			</li>
-			<li>
-			"+1 for Martin."
-			</li>
-			<li>
-			"I give my +1 to Martin Oberhuber. The work he's doing as Architecture Council
-			lead is priceless."
+			Comment 2
 			</li>
 		</ul>
 		</p>
 		</div>	
 
-		<p><a href="javascript:switchMenu('SteffenPingel');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Steffen Pingel
-		</p>
-		<div id="SteffenPingel" class="switchcontent">
 		<p>
-		<ul>
-			<li>
-			"I nominate Steffen Pingel for his contributions to the Mylyn project, in the
-			form of community support via countless bugs and posts, as well as his code
-			contributions.  I hate to say this, but Steffen had more than 1/2 of the
-			commits on Mylyn last year.  To put that into perspective, Dash stats for 2008
-			put Mylyn as the most active Tools project by a pretty wide margin. His
-			increasing involvement with cross-project, build and install has also been
-			helpful in the release trains."	
-			</li>
-		</ul>
-		</p>
-		</div>	
-
-		<p><a href="javascript:switchMenu('ThomasWatson');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Thomas Watson
-		</p>
-		<div id="ThomasWatson" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I nominate Thomas Watson, the proverbial man behind the Equinox curtain. For
-			those who don't know, Tom has been responsible for the Equinox framework for
-			quite awhile, even back when it had a trendy name like "SMF" and OSGi was
-			"revolutionizing" the home automation market. 
-			<br><br>
-			Tom's a quiet guy, but his careful eye in keeping the framework stable and
-			pushing it to Eclipse gets a nod from me. On top of that, he represents Eclipse
-			on the EEG and CPEG committees at OSGi.org..., he's even promised me that the
-			EEG won't ruin OSGi ;)
-			<br><br>
-			Ok, +1 for Thomas Watson"
-			</li>
-		</ul>
-		</p>
-		</div>			
-
-		<p><a href="javascript:switchMenu('JasonWeathersby');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Jason Weathersby
-		</p>
-		<div id="JasonWeathersby" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I would like to nominate Jason Weathersby as Top Committer for his work on the
-			BIRT Project.  Jason's work through the PMC, writing papers, speaking at
-			conferences, maintaining our website, and most importantly monitoring our
-			newsgroup have been a key reason for that people have taken up BIRT.
-			<br><br>
-			The BIRT newsgroup is one of the busiest, most successful newsgroups in the
-			Eclipse community.  Jason has been instrumental in making this happen.  Jason
-			has written more than 5,000 newsgroup posts (16% of the total newsgroup
-			volume).    All of the as answers or debug on customer posed questions.
-			<br><br>
-			He has helped our community work through some incredibly difficult and complex
-			issues.  All of this without adopting any of the attitude that some experts
-			seem to get."	
-			</li>
-			<li>
-			"+1, I do agree with Scott, Jason Weathersby is really professional and warm
-			hearted, every body including me can get quick reply and exact answer from
-			Jason."
-			</li>
-			<li>
-			"i would like to nominate Jason all the time..coz he is very prompt & patient
-			and you can expect solutions very quick. He is really the best."
-			</li>
-			<li>
-			"+ 1 for Jason, Jason is the man."
-			</li>
-			<li>
-			"+ 1 for Jason Weathersby ,  he deserves it , fighting!!!"
-			</li>
-			<li>
-			"+1 for Jason Weathersby. Anytime I've had questions I've got quick and detailed
-			answers on my questions. His commitment to the BIRT project and the community
-			cannot be understated.
-			Keep it up Jason"
-			</li>
-		</ul>
-		</p>
-		</div>	
-					
-		<p><a href="javascript:switchMenu('PaulWebster');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			Paul Webster
-		</p>
-		<div id="PaulWebster" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I nominate Paul Webster for his tireless efforts in helping people on IRC,
-			newsgroups and elsewhere."
-			</li>
-			<li>
-			"+1. What Chris said - Paul is an Eclipse machine.  A bitter, surly Eclipse
-			machine.&nbsp;&nbsp;:)&nbsp;"
-			</li>
-			<li>
-			"+1 for Paul, this is a no-brainer."
-			</li>
-			<li>
-			"+1 for Paul"
-			</li>
-		</ul>
-		</p>
-		</div>
-
-		<p><a href="javascript:switchMenu('DavidWilliams');" title="Description">
-		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description"></a>
-			David Willilams
-		</p>
-		<div id="DavidWilliams" class="switchcontent">
-		<p>
-		<ul>
-			<li>
-			"I nominate David Williams for his contributions to the Eclipse community. He
-			does a lot of important work from triage to release engineering that is
-			generally considered behind the scenes. He's managed to help grow the Webtools
-			project and establish a successful incubator within Webtools. He's also been
-			involved in Orbit and things like the release trains. Without David, it
-			wouldn't have been possible for us to have a simultaneous release."	
-			</li>
-		</ul>
-		</p>
-		</div>		
-
-		<p>
-		To read more about the candidates, click on the plus sign beside each name and
-		<a href="http://eclipse.dzone.com/articles/meet-years-top-committer" target="blank">see the DZone interview</a> with the Top Committer nominees.
+		To read more about the candidates, click on the plus sign beside each name.
 		</p>		
 		
 		<h3>Top Contributor</h3>
@@ -568,8 +275,7 @@ function switchMenu(obj) {
 		</div>
 		
 		<p>
-		To read more about the candidates, click on the plus sign beside each name and
-		<a href="http://eclipse.dzone.com/articles/meet-years-top-contributor" target="blank">see the DZone interview</a> with the Top Contributor nominees.
+		To read more about the candidates, click on the plus sign beside each name.
 		</p>
 	</div>
 
@@ -579,13 +285,11 @@ function switchMenu(obj) {
 			<h6>Related Links</h6>
 			<ul>
 				<li><a href="index.php">Eclipse Community Awards</a></li>
-				<li><a href="winners09.php">2009 Winners</a></li>
-				<li><a href="judges09.php">2009 Judges</a></li>
-				<li><a href="individual.php">Individual Nominations</a></li>
-				<li><a href="technology.php">Technology Nominations</a></li>
-				<li><a href="pastwinners.php">Past Winners</a></li>
 				<li><a href="nominate_individual.php">Make an Individual Nomination</a></li>
 				<li><a href="nominate_technology.php">Make a Technology Nomination</a></li>
+				<li><a href="individual.php">Individual Nominees</a></li>
+				<li><a href="technology.php">Technology Nominees</a></li>
+				<li><a href="pastwinners.php">Past Winners</a></li>
 			</ul>
 		</div>
 	</div>
