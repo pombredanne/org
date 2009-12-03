377a9625f0b53b5d078a6f8397d26399fa59be32
lgayowski <lgayowski>
2009-12-03 20:19:41 +0000

    *** empty log message ***

--- foundation/eclipseawards/index.php
+++ foundation/eclipseawards/index.php
@@ -107,9 +107,9 @@
 			<h6>Related Links</h6>
 			<ul>
 				<li><a href="index.php">Eclipse Community Awards</a></li>
-				<li><a href="nominate_individual.php">Make an Individual Nominations</a></li>
-				<li><a href="nominate_project.php">Make a Project Nominations</a></li>				
-				<li><a href="nominate_technology.php">Make a Technology Nominations</a></li>
+				<li><a href="nominate_individual.php">Make an Individual Nomination</a></li>
+				<li><a href="nominate_project.php">Make a Project Nomination</a></li>				
+				<li><a href="nominate_technology.php">Make a Technology Nomination</a></li>
 				<li><a href="pastwinners.php">Past Winners</a></li>
 			</ul>
 		</div>
