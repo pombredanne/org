2bf1e84712326fff2de6f0f0eeb1908847640626
lgayowski <lgayowski>
2010-01-12 22:28:25 +0000

    *** empty log message ***

--- foundation/eclipseawards/project.php
+++ foundation/eclipseawards/project.php
@@ -117,6 +117,22 @@ function switchMenu(obj) {
 
 		<h3>Most Open Project</h3>
 
+		<p><a href="javascript:switchMenu('p2');" title="Description">
+		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description">
+			Equinox p2</a>
+		</p>
+		<div id="p2" class="switchcontent">
+		<p>
+		<ul>
+			<li>
+			I'd like to nominate the RT Equinox p2 project as one of the most open projects.  This project has new committers that have come on board from EclipseSource, Cloudsmith and an individual in the academic community (Universit&eacute; d'Artois).  
+			<br></br>
+			They are very responsive and helpful to questions on the mailing list. They have had a number of blog entries on p2 that have been very useful to the community (I've learned a lot :-)
+			</li>
+		</ul>
+		</p>
+		</div>
+
 		<p><a href="javascript:switchMenu('Mylyn');" title="Description">
 		<img src="http://www.eclipse.org/newsgroups/images/plus.gif" alt="Description" title="Description">
 			Mylyn</a>
