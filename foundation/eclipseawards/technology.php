c790a01462ef36ce3d338211263428c2c01f1185
lgayowski <lgayowski>
2010-02-02 16:05:56 +0000

    *** empty log message ***

--- foundation/eclipseawards/technology.php
+++ foundation/eclipseawards/technology.php
@@ -53,6 +53,7 @@ ob_start();
 				<li><a href="http://www.model2code.com" target="blank">M2Flex</a></li>
 				<li><a href="http://www.nwiresoftware.com/" target="blank">nWire</a></li>
 				<li><a href="http://www.oxygenxml.com" target="blank">&lt;oXygen/&gt; XML Editor</a></li>
+				<li><a href="http://www.springsource.com/products/suite/sts" target="blank">SpringSource Tool Suite</a></li>
 				<li><a href="http://www.instantiations.com/windowtester/index.html" target="blank">WindowTesterPro</a></li>
 				<li><a href="http://wireframesketcher.com" target="blank">WireframeSketcher</a></li>
 				<li><a href="http://www.zend.com/products/studio" target="blank">Zend Studio 7</a></li>
