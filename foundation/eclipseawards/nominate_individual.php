28c011d62749696ef3ed8e8c4a720981ff27520d
lgayowski <lgayowski>
2010-02-02 16:50:37 +0000

    *** empty log message ***

--- foundation/eclipseawards/nominate_individual.php
+++ foundation/eclipseawards/nominate_individual.php
@@ -38,7 +38,7 @@
 		<p>
 			Individuals may be nominated by anyone in each category.  All that's required to make a nomination
 			is to list the individual's name and a paragraph on why you think they are worthy of an award in
-			the appropriate Bugzilla entry link below. Nominations opened December 3, 2009 and will close January 29,
+			the appropriate Bugzilla entry link below. Nominations opened December 3, 2009 and closed January 29,
 			2010.  Winners will be announced at <a href="http://www.eclipse.org/go/EC10_web@awards">EclipseCon 2010</a>.
 		</p>
 
@@ -55,7 +55,7 @@
 		<p>
 		The Individual Award winners will be determined by a vote of the community. Each person will get
 		to vote once per category. After nominations close on January 29, 2010, voting will run from
-		February 1-26, 2010, 5:00 pm EST.  
+		February 2-26, 2010, 5:00 pm EST.  
 		</p>
 		
 		<h2>Past Winners</h2>
