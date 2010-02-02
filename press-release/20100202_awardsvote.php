b21c6925eff92e18aece2a6df194bcd81c6369aa
lgayowski <lgayowski>
2010-02-02 21:21:46 +0000

    *** empty log message ***

--- press-release/20100202_awardsvote.php
+++ press-release/20100202_awardsvote.php
@@ -109,14 +109,14 @@ function switchMenu(obj) {
 		<div class="sideitem">
 			<h6>Related Links</h6>
 			<ul>
-				<li><a href="index.php">Eclipse Community Awards</a></li>
-				<li><a href="individual.php">Individual Nominees</a></li>
-				<li><a href="project.php">Project Nominees</a></li>
-				<li><a href="technology.php">Technology Nominees</a></li>
-				<li><a href="pastwinners.php">Past Winners</a></li>
-				<li><a href="nominate_individual.php">Make an Individual Nomination</a></li>
-				<li><a href="nominate_project.php">Make a Project Nomination</a></li>				
-				<li><a href="nominate_technology.php">Make a Technology Nomination</a></li>
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/index.php">Eclipse Community Awards</a></li>
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/individual.php">Individual Nominees</a></li>
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/project.php">Project Nominees</a></li>
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/technology.php">Technology Nominees</a></li>
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/pastwinners.php">Past Winners</a></li>
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/nominate_individual.php">Make an Individual Nomination</a></li>
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/nominate_project.php">Make a Project Nomination</a></li>				
+				<li><a href="http://www.eclipse.org/org/foundation/eclipseawards/nominate_technology.php">Make a Technology Nomination</a></li>
 			</ul>
 		</div>
 	
