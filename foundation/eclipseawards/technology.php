a77f79a1644f0459c206b1b11dd3b652dca77afe
lgayowski <lgayowski>
2010-02-03 19:41:40 +0000

    *** empty log message ***

--- foundation/eclipseawards/technology.php
+++ foundation/eclipseawards/technology.php
@@ -85,6 +85,7 @@ ob_start();
 			<h3>Best RCP Application</h3>
 			<ul>
 				<li><a href="http://www.bioclipse.net" target="blank">Bioclipse</a></li>
+				<li><a href="http://www.bonitasoft.com/products/overview.php" target="blank">Bonita Open Solution 5.0</a></li>
 				<li><a href="http://www.ditaworks.com" target="blank">DITAworks Eclipse Help Package</a></li>
 				<li><a href="http://jcryptool.sourceforge.net" target="blank">JCryp Tool</a></li>
 				<li><a href="http://www.postergenius.com" target="blank">PosterGenius</a></li>
