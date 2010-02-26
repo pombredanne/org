963e6e3eabd452f80600c9699becdff9c168af21
lgayowski <lgayowski>
2010-02-26 22:04:39 +0000

    *** empty log message ***

--- foundation/eclipseawards/technology.php
+++ foundation/eclipseawards/technology.php
@@ -35,7 +35,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		
 		<p>
-			The following is a list of nominations submitted for the Technology Awards.  Nominations closed on January 29, 2010 and submissions were sent to a judging panel.
+			The following is a list of nominations submitted for the Technology Awards.  Nominations closed on January 29, 2010 and submissions were sent to a judging panel. Winners will be announced March 22 at EclipseCon.
 		</p>
 		
 		<div class="homeitem3col">
