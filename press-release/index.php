9c73f39625ec37660f2ebf180af6e3d1cb84c273
lgayowski <lgayowski>
2010-03-08 14:20:48 +0000

    *** empty log message ***

--- press-release/index.php
+++ press-release/index.php
@@ -36,6 +36,7 @@ ob_start();Tha
 		<h2>Current Press Releases</h2>
 		<hr size="1" />
 		<ul>
+		<li><a href="20100308_mylyn.php">Mylyn Enables Agile Aim for Eclipse</a> - March 8, 2010</li>
 		<li><a href="20100305_awardsfinalists.php">Eclipse Awards Finalists 2010</a> - March 5, 2010</li>
 		<li><a href="20100224_e4Rover.php">e4-Rover Mars Challenge at EclipseCon</a> - February 24, 2010</li>
 		<li><a href="20100223_memberoptions.php">Eclipse Foundation Broadens Participation Options for Community Members</a> - February 23, 2010</li>
