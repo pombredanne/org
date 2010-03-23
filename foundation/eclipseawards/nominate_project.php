77b815d67255804dc3db21566db94607016bd3e8
lgayowski <lgayowski>
2010-03-23 01:22:31 +0000

    *** empty log message ***

--- foundation/eclipseawards/nominate_project.php
+++ foundation/eclipseawards/nominate_project.php
@@ -41,7 +41,7 @@
 			required to make a nomination is to list the project or feature and a paragraph on why you
 			think it's worthy of an award in the appropriate Bugzilla entry link below. The Most Innovative New Feature or Project 
 			award is for features and new projects introduced in 2009.  Nominations opened
-			December 3, 2009 and closed January 29,	2010.  Winners will be announced at
+			December 3, 2009 and closed January 29,	2010.  Winners were announced at
 			<a href="http://www.eclipse.org/go/EC10_web@awards">EclipseCon 2010</a>.
 		</p>
 
@@ -71,11 +71,12 @@
 			<h6>Related Links</h6>
 			<ul>
 				<li><a href="index.php">Eclipse Community Awards</a></li>
-				<li><a href="finalists10.php">Finalists</a></li>
+				<li><a href="winners10.php">2010 Winners</a></li>
+				<li><a href="judges10.php">2010 Judges</a></li>
+				<li><a href="pastwinners.php">Past Winners</a></li>
 				<li><a href="individual.php">Individual Nominees</a></li>
 				<li><a href="project.php">Project Nominees</a></li>
 				<li><a href="technology.php">Technology Nominees</a></li>
-				<li><a href="pastwinners.php">Past Winners</a></li>
 				<li><a href="nominate_individual.php">Make an Individual Nomination</a></li>
 				<li><a href="nominate_project.php">Make a Project Nomination</a></li>				
 				<li><a href="nominate_technology.php">Make a Technology Nomination</a></li>
