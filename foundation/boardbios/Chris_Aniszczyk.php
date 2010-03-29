8c1907797c29327313f2cd5db65f38d9c6f3fbe7
ajacko <ajacko>
2010-03-29 22:22:49 +0000

    Updated Chris's bio at his request.

--- foundation/boardbios/Chris_Aniszczyk.php
+++ foundation/boardbios/Chris_Aniszczyk.php
@@ -1,8 +1,9 @@
 <img src="boardbios/photos/Chris_Aniszczyk.jpg" align="left" style="padding-right: 5px;" width="120">
-Chris Aniszczyk is the technical lead for the Eclipse Plug-in Development 
-Environment (PDE) project and a senior software engineer at EclipseSource. 
-He tends to be all over the place inside the Eclipse community by committing 
-to and mentoring various Eclipse projects. He sits on the Eclipse Architecture 
-Council and on the Eclipse Technology PMC. His passions are blogging, 
-software advocacy, tooling, and anything Eclipse. He's always available 
-to discuss open source or Eclipse over a frosty beverage.
+Chris Aniszczyk is the technical lead for the Eclipse Plug-in
+Development Environment (PDE) project and a Principal Software
+Engineer at Red Hat. He tends to be all over the place inside the
+Eclipse community by committing to and mentoring various Eclipse
+projects. He sits on the Eclipse Architecture Council and on the
+Eclipse Technology PMC. His passions are blogging, software advocacy,
+tooling and anything Eclipse. He's always available to discuss open
+source or Eclipse over a frosty beverage.
