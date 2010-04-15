bdd8422a6c63f78ae4b8381337f0b29eb3961722
lgayowski <lgayowski>
2010-04-15 17:56:40 +0000

    *** empty log message ***

---
+++ foundation/boardbios/Adam_Lieber.php
@@ -0,0 +1,6 @@
+<img src="boardbios/photos/Adam_Lieber.jpg" align="left" style="padding-right: 5px;" width="120">
+Adam is General Manager of IntalioWorks, which includes the BPMS, Jetty and Cometd offerings from Intalio, and
+operates with a commercial open source model in 15 countries around the world. Previously, he was CEO of Webtide,
+which focused on services for Jetty and Cometd, where he helped bring the Jetty project to the Eclipse Foundation
+Runtime project. Previously, he was a co-founder of Gluecode Software, a pioneer in commercial open source
+offerings, which was acquired by IBM, where he lead worldwide sales for the group. 
