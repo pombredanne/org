1678599dae08189993d4c70a9816f2e568535d42
lgayowski <lgayowski>
2010-04-15 18:16:46 +0000

    *** empty log message ***

---
+++ foundation/boardbios/Jason_Van Zyl.php
@@ -0,0 +1,10 @@
+<img src="boardbios/photos/Jason_Van_Zyl.jpg" align="left" style="padding-right: 5px;" width="120">
+Jason van Zyl is CTO of Sonatype and the founder of the Apache Maven project, the Plexus IoC framework and the
+Apache Velocity project. Jason currently serves on the Apache Maven Project Management Committee. He has been
+involved with the Apache Software Foundation (ASF) for seven years, helped to found Codehaus, a well respected
+incubation facility for open source community projects, and is a frequent speaker at many major software
+conferences, including JavaOne, EclipseCon, EmergingTech and ApacheCon. Jason has over ten years of enterprise
+software development experience. He founded Periapt, Inc., a company that provided software infrastructure
+development services to Fortune 500 companies such as Toyota Corp., Bank of America and Coca-Cola Co. Previous
+to Periapt, he worked as a Technology Architect at Compusense, a world leader in sensory analysis and data
+research.
