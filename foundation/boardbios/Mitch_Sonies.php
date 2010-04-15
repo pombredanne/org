a34ea37bdbc0ff7b8c7eb3a781bdea75aaea8031
lgayowski <lgayowski>
2010-04-15 18:08:10 +0000

    *** empty log message ***

--- foundation/boardbios/Mitch_Sonies.php
+++ foundation/boardbios/Mitch_Sonies.php
@@ -1,3 +1,4 @@
+<img src="boardbios/photos/Mitch_Sonies.jpg" align="left" style="padding-right: 5px;" width="120">
 Mitch is a co-founder and CEO of Cloudsmith. Mitch is currently on his third career. In his first, he was a
 lawyer specializing in venture funds and financings (i.e. the sidelines). In his second, he was a Wall Street
 research analyst (i.e. the press box). In his third career, Mitch took the last step onto the playing field as
