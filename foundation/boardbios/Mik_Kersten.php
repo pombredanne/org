e646d2b2b47af78cd0d8c9d0f7efa932a485a839
ajacko <ajacko>
2010-04-21 18:11:10 +0000

    Updated bio at Mik's request.

--- foundation/boardbios/Mik_Kersten.php
+++ foundation/boardbios/Mik_Kersten.php
@@ -1,6 +1,10 @@
 <img src="boardbios/photos/Mik_Kersten.jpg" align="left" style="padding-right: 5px;" width="120">
-Dr. Mik Kersten is the CEO of Tasktop Technologies and lead of the Eclipse Mylyn open source project. As a
-research scientist at Xerox PARC, Mik implemented the first aspect-oriented programming tool support. He then
-invented the task-focused interface and created Mylyn as part of his PhD at the University of British Columbia.
-Mik is a popular speaker at software conferences and was voted a JavaOne Rock Star speaker in 2008 and 2009.
-He enjoys building tools that offload our brains and make it easier to get creative work done.
+
+Dr. Mik Kersten is the CEO of Tasktop Technologies, creator of the Eclipse Mylyn open source project and inventor 
+of the task-focused interface. As a research scientist at Xerox PARC, Mik implemented the first aspect-oriented programming 
+tools for AspectJ. He created Mylyn and the task-focused interface during his PhD in Computer Science at the University of 
+British Columbia. Mik has been an Eclipse committer since 2002, is an elected member of the Eclipse Board of Directors and 
+serves on the Eclipse Architecture Council. Mik's thought leadership on task-focused collaboration makes him a popular 
+speaker at software conferences, and he was voted a JavaOne Rock Star speaker in 2008 and 2009. Mik has also been recognized 
+as one of the top ten IBM developerWorks Java technology writers of the decade. He enjoys building tools that offload our 
+brains and make it easier to get creative work done. Follow <a href="http://twitter.com/mik_kersten">@mik_kersten on Twitter</a>.
