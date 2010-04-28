5a6fa90f2231bb5b4692abda12a8f62113cd1a02
ngervais <ngervais>
2010-04-28 18:06:00 +0000

    Removed Old Stuff

--- newindex.php
+++
@@ -1,8 +0,0 @@
-<html>
-   <head>
-      <meta http-equiv="refresh" content="5; url=../membership/become_a_member/" />
-   </head>
-   <body>
-      <a href="../membership/become_a_member/">This page has moved. Please update your links and bookmarks.</a>
-   </body>
-</html>
