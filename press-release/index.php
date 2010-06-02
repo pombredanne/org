36ced896a5f3c38f9f2a5f9ea814c70da16319e8
lgayowski <lgayowski>
2010-06-02 19:13:11 +0000

    *** empty log message ***

--- press-release/index.php
+++ press-release/index.php
@@ -36,6 +36,8 @@ ob_start();Tha
 		<h2>Current Press Releases</h2>
 		<hr size="1" />
 		<ul>
+		<li><a href="20100602_heliosblogathon.php">Write a Review for the Helios Blogathon</a> - June 2, 2010</li>
+		<li><a href="20100525_ese2010cfp.php">Eclipse Summit Europe 2010 Call for Participation</a> - May 25, 2010</li>
 		<li><a href="20100504_democamps.php">Celebrate Helios at an Eclipse DemoCamp</a> - May 4, 2010</li>
 		<li><a href="20100421_soaiwg.php">Eclipse Announces New SOA Initiative</a> - April 21, 2010</li>
 		<li><a href="20100415_BankingDay.php">Register for Eclipse Banking Day 2011</a> - April 15, 2010</li>
