a81463e420873ecd0b707880517b1d121b2feb71
lgayowski <lgayowski>
2010-11-05 16:56:06 +0000

    *** empty log message ***

--- press-release/20101103_EclipseConAuditions.php
+++ press-release/20101103_EclipseConAuditions.php
@@ -34,13 +34,15 @@
 		<p>The auditions will be held using the <a href="http://en.wikipedia.org/wiki/Ignite_%28event%29">
 		Ignite format</a>. Each speaker will have 5 minutes to present 20 slides, and the slides change
 		automatically every 15 seconds. The audition sessions will occur on November 18 and be broadcast via
-		a <a href="https://www2.gotomeeting.com/register/646158018">webinar</a>. To apply for an audition you
+		a <a href="https://www2.gotomeeting.com/register/646158018">webinar</a>.  Your presentation should be a
+		condensed version of your proposed content rather than background on the target audience or session goals.
+		To apply for an audition you
 		need to:<br>
 		1. <a href="http://www.eclipsecon.org/2011/submissions/?page=submissions">Submit an EclipseCon proposal</a><br>
 		2. Send an e-mail to <a href="mailto:audition@eclipse.org">audition@eclipse.org</a> with your name and
 		the link to your proposal<br><br>
-		The deadline to apply for an audition is November 15, 2010. The winner of the
-		EclipseCon Audition Sessions will receive a speaker slot at EclipseCon.</p>
+		The deadline to apply for an audition is November 15, 2010 and your slides must be submitted by November
+		17, 2010. The winner of the EclipseCon Audition Sessions will receive a speaker slot at EclipseCon.</p>
 		
 		<p>As a reminder, November 17, 2010 is the deadline to be considered for one of EclipseCon's top five
 		early picks. The program committee will select 5 speakers from the proposals submited before November 17.
