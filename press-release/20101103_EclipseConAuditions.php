d7c4768dc3232ae9c783c2420fe38ec4b72108c7
lgayowski <lgayowski>
2010-11-08 19:11:05 +0000

    *** empty log message ***

--- press-release/20101103_EclipseConAuditions.php
+++ press-release/20101103_EclipseConAuditions.php
@@ -57,9 +57,11 @@
 		<div class="sideitem">
 			<h6>Related Links</h6>
 			<ul>
+				<li><a href="20101103_EclipseConAuditions.php">What are EclipseCon Auditions?</a></li>							
 				<li><a href="https://www2.gotomeeting.com/register/646158018">Watch the EclipseCon Auditions</a></li>
+				<li><a href="../../community/EclipseConAuditions2010.php">EclipseCon Audition Agenda</a></li>
 				<li><a href="http://www.eclipsecon.org/2011/submissions/?page=submissions">Propose an EclipseCon Talk</a></li>
-				</ul>
+			</ul>
 		</div>
 	</div>
 </div>
