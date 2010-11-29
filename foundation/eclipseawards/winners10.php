a4328d5ab9e05f77e5da40179ee3dd46a32f5167
lgayowski <lgayowski>
2010-11-29 19:58:27 +0000

    *** empty log message ***

--- foundation/eclipseawards/winners10.php
+++ foundation/eclipseawards/winners10.php
@@ -131,6 +131,10 @@ ob_start();
 			<h6>Awards Navigation</h6>
 			<ul>
 				<li><a href="index.php">Eclipse Community Awards</a></li>
+				<li><a href="individual.php">Individual Nominees</a></li>
+				<li><a href="project.php">Project Nominees</a></li>
+				<li><a href="technology.php">Technology Nominees</a></li>
+				<li><a href="pastwinners.php">Past Winners</a></li>
 			</ul>
 		</div>
 	</div>
