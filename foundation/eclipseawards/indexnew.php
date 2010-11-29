87d24bdcd37d2bd427807ce8af2138e92cd9b378
lgayowski <lgayowski>
2010-11-29 16:37:37 +0000

    *** empty log message ***

---
+++ foundation/eclipseawards/indexnew.php
@@ -0,0 +1,140 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# sample_list.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-11-07
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Eclipse Community Awards";
+	$pageKeywords	= "eclipse, awards, community, nomination, eclipsecon, 2011";
+	$pageAuthor		= "Lynn Gayowski";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 1);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 1);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		
+		<p>
+			The Eclipse Community Awards recognize the best of the Eclipse community.
+		</p>
+		
+		<h3>Individual Awards</h3>	
+		<h4>Description</h2>
+		<h4>Nominate</h2>
+		<h4>Current Nominees</h2>
+		<h4>Past Winners</h2>
+		
+		<h3>Project Awards</h3>
+		<h4>Description</h2>
+		<h4>Nominate</h2>
+		<h4>Current Nominees</h2>
+		<h4>Past Winners</h2>
+		
+		<h3>Technology Awards</h3>
+		<h4>Description</h2>
+		<h4>Nominate</h2>
+		<h4>Current Nominees</h2>
+		<h4>Past Winners</h2>		
+		
+		<p>
+			<ol>
+				<li><strong>Top Committer</strong> - The Eclipse committer who best exemplifies support for the
+				community through forums, Bugzilla, tutorials, conference presentations, blogs and other areas.</li>
+				<li><strong>Top Contributor</strong> - The individual who best exemplifies support for
+				the Eclipse community through forums, submission of patches, comments on bugs, tutorials, conference
+				presentations, blogs, IRC and other areas. Contributors are the individuals that participate due to their
+				passion for the community and technology.  Typically, they are not paid for their
+				Eclipse contributions.</li>
+ 				<li><strong>Top Newcomer Evangelist</strong> - The individual who best exemplifies the welcoming
+ 				of new people into the Eclipse community, such as through answering questions on the Newcomers forum,
+ 				blogging, creating resources like demos and tutorials, participating in Eclipse groups on social media
+ 				sites and helping on IRC.</li>	
+			</ol> 	
+			<a href="individual.php">Make an individual award nomination</a>
+			<br>
+		</p>
+		
+		<h3>Eclipse Project Awards</h3>
+		<p>
+			<ol>						
+				<li><strong>Most Innovative New Feature or Eclipse Project ("Shiny New Toy Award")</strong> - 
+				A feature added to an existing Eclipse project in 2010 or a new Eclipse project
+				introduced in 2010 that is used and loved by the community.</li>
+				<li><strong>Most Open Project</strong> - An Eclipse project that best exemplifies the openness,
+				transparency and diversity expected of great open source projects. </li>	
+				</ol>
+			<a href="project.php">Make a project award nomination</a>
+			<br>
+
+		</p>
+		
+		<h3>Technology Awards</h3>
+		<p>
+			<ol>						
+				<li><strong>Best Commercial Developer Tool</strong> - A commercial product that provides an
+				innovative, high quality tool for software developers.</li>
+				<li><strong>Best Open Source Developer Tool</strong> - An open source project (not hosted at
+				Eclipse) that provides an innovative, high quality tool for software developers.</li>
+				<li><strong>Best EclipseRT Application</strong> - An application, commercial product or open
+				source project (not hosted at Eclipse) that illustrates the most innovative use of EclipseRT technology.</li>
+				<li><strong>Best RCP Application</strong> - An end-user desktop application (not hosted at
+				 Eclipse) that illustrates the most innovative use of Eclipse RCP.</li>
+			</ol>
+			<a href="technology.php">Make a technology award nomination.</a>
+			<br>
+		</p>
+		
+		<h3>Important Dates</h3>
+		<p>
+			<ul>
+				<li><font color="#A9A9A9">Nominations open - December 6, 2010</font></li>
+				<li>Nominations close - January 28, 2011</font></li>
+				<li>Voting begins for Individual & Project Awards - January 31, 2011</font></li>
+				<li>Voting ends for Individual & Project Awards - February 25, 2011 at 5:00 pm EST</font></li>
+				<li>Awards Announced at EclipseCon 2010 - March 21, 2011</li>
+			</ul>
+		</p>
+		
+	</div>
+
+	<!-- remove the entire <div> tag to omit the right column!  -->
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Related Links</h6>
+			<ul>
+				<li><a href="indexnew.php">Eclipse Community Awards</a></li>
+				<li><a href="nominatenew.php">Make a Nomination</a></li>
+				<li><a href="individual.php">Individual Nominees</a></li>
+				<li><a href="project.php">Project Nominees</a></li>
+				<li><a href="technology.php">Technology Nominees</a></li>
+				<li><a href="pastwinners.php">Past Winners</a></li>	
+			</ul>
+		</div>
+		
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
