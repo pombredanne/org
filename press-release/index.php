3d7e570fe9a0fc39b8dced92e9dc2baee0b0b281
lgayowski <lgayowski>
2010-12-02 18:45:33 +0000

    *** empty log message ***

--- press-release/index.php
+++ press-release/index.php
@@ -36,6 +36,7 @@ ob_start();Tha
 		<h2>Current Press Releases</h2>
 		<hr size="1" />
 		<ul>
+		<li><a href="20101202_EclipseConAuditions2.php">EclipseCon Audition Sessions, Take 2</a> - December 2, 2010</li>
 		<li><a href="20101103_EclipseConAuditions.php">Announcing the EclipseCon Audition Sessions</a> - November 3, 2010</li>
 		<li><a href="20101102_virgo.php">Eclipse Makes Available New Release of Eclipse Virgo</a> - November 2, 2010</li>
 		<li><a href="20101101_eclipsedayvalencia.php">Sign up to Attend EclipseDay in Valencia</a> - November 1, 2010</li>
