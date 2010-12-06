7991176307b6661b7cc589c1c710f8f67b236282
lgayowski <lgayowski>
2010-12-06 15:50:48 +0000

    *** empty log message ***

--- foundation/eclipseawards/winners_project.php
+++ foundation/eclipseawards/winners_project.php
@@ -53,6 +53,7 @@ ob_start();
 			<h6>Related Links</h6>
 			<ul>
 				<li><a href="index.php">Eclipse Community Awards</a></li>
+				<li><a href="pastwinners.php">Past Winners</a></li>
 			</ul>
 		</div>
 	</div>
