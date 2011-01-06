6373ae6cfdaa5db2f2d89ecd224bdc1ddaed9ca4
lgayowski <lgayowski>
2011-01-06 21:38:09 +0000

    *** empty log message ***

---
+++ foundation/boardbios/Etienne_Juliot.php
@@ -0,0 +1,5 @@
+<img src="boardbios/photos/Etienne_Juliot.jpg" align="left" style="padding-right: 5px;" width="120">
+Etienne is one of the major contributors for the SCA subproject in STP, and he is involved in the Eclipse SOA IWG.
+Etienne is one the founders of Obeo, focusing on Model Driven tools. He contributes to several Eclipse projects
+(Acceleo, ATL, EEF, ...) and to several Open Source communities. He also drives the commercial strategy of Obeo
+on Eclipse Modeling technologies for reverse enginnering of legacy code and viewpoint modeling.
