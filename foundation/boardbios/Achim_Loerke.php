24443a6f282438358679a80b5d453b33341bc599
lgayowski <lgayowski>
2011-01-06 21:34:48 +0000

    *** empty log message ***

---
+++ foundation/boardbios/Achim_Loerke.php
@@ -0,0 +1,4 @@
+<img src="boardbios/photos/Achim_Loerke.jpg" align="left" style="padding-right: 5px;" width="120">
+Achim is a software developer and architect at heart. As a managing director at BREDEX GmbH he is responsible
+for technology and development strategies. In addition he is the project lead of the Eclipse Jubula project and
+represents his company in the Eclipse foundation. 
