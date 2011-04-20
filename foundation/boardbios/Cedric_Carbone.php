c5c95dcfdf1014ac37410a0d3c976d7b4b22a5a2
ajacko <ajacko>
2011-04-20 16:10:04 +0000

    Added Board member.

---
+++ foundation/boardbios/Cedric_Carbone.php
@@ -0,0 +1,5 @@
+<img src="boardbios/photos/Cedric_Carbone.jpg" align="left" style="padding-right: 5px;" width="120">
+C&eacute;dric Carbone is Talend's Chief Technical Officer and Board Member of OW2 consortium (since the inception). Prior to 
+joining Talend in 2006, he managed the Java practice at Neurones, a leading systems integrator in Europe. C&eacute;dric 
+has also lectured at several universities on technical topics such as XML or Web Services. He holds a master's degree in 
+Computer Science and an advanced degree in Document Engineering.
