9e107a8704594bfecdcc11782d18aca58914255b
wbeaton <wbeaton>
2011-05-08 04:13:20 +0000

    Minor format updates.

--- foundation/staff.php
+++ foundation/staff.php
@@ -1,50 +1,59 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-
-	#*****************************************************************************
-	#
-	# template.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
+/*******************************************************************************
+* Copyright (c) 2005, 2011 Eclipse Foundation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Wayne Beaton (Eclipse Foundation) - initial API and implementation
+*******************************************************************************/
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
 	
-	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Eclipse Foundation Staff";
-	$pageKeywords	= "foundation, staff";
-	$pageAuthor		= "Wayne Beaton";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 1);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 1);
+$App = new App();
+$Nav = new Nav();
+$Menu = new Menu();
 
-	# End: page-specific settings
-	#
-	include("scripts/staff_xml.php");
-	$staff = get_staff_as_html();
-	
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+include($App->getProjectCommon());
 
+$pageTitle 		= "Eclipse Foundation Staff";
+$pageKeywords	= "foundation, staff";
+$pageAuthor		= "Wayne Beaton";
+
+function dumpStaffInfo() {
+	$staff = simplexml_load_file(dirname(__FILE__) . '/staff.xml');
+	echo "<div class=\"homeitem\">";
+	foreach ($staff as $member) {
+		echo "<h3><a name=\"$member->id\"></a>$member->name</h3>";
+		if ($member->image)
+			echo "<img src=\"$member->image\" align=\"right\">";
+		if ($member->title)
+			echo "<p><em>$member->title</em></p>";
+
+		echo "<blockquote>$member->description</blockquote>";
+	}
+	echo "</div>";
+}
+
+ob_start();
+?>
 	<div id="midcolumn">
-		<h1>$pageTitle</h1>
+		<h1><?php echo $pageTitle ?></h1>
 		<p>The Eclipse Foundation is fortunate to have some very talented people working
 		full-time on behalf of the Eclipse community. Below is the list of our staff.</p>
 		<p>Eclipse email addresses all follow the firstname.lastname@eclipse.org convention.</p>
-		<div class="homeitem3col">
-			<h3>Foundation Staff</h3>
-			$staff
-		</div>
+		
+		<?php dumpStaffInfo(); ?>
 	</div>
 
-EOHTML;
-	$html = mb_convert_encoding($html, "HTML-ENTITIES", "auto");
+<?php
+	# Paste your HTML content between the EOHTML markers!
+	$html = ob_get_contents();
+	ob_end_clean();
+
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
