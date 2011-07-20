2e2bb4d20b73accb01059e6d08e994a2beda5828
ajacko <ajacko>
2011-07-20 18:03:13 +0000

    *** empty log message ***

--- press-release/20110721_java7summit.php
+++ press-release/20110721_java7summit.php
@@ -28,19 +28,19 @@
 		<h1>$pageTitle</h1>
 		
 		<br>
-  <p>The Eclipse Foundation, in collaboraiton with Oracle, is pleased to announce the Java 7 Summit, which will be held at the upcoming <a href="http://www.eclipsecon.org/europe2011/">EclipseCon Europe</a> in Ludwigsburg Germany on 
-  November 2-4, 2011.  The Java 7 Summit will offer Java developers and architects an opportunity to gain in-depth technical education on the new innovations introduced into the recent 
+  <p>The Eclipse Foundation, in collaboraiton with Oracle, is pleased to announce the Java 7 Summit, to be held at the upcoming <a href="http://www.eclipsecon.org/europe2011/">EclipseCon Europe</a> in Ludwigsburg, Germany on 
+  November 2-4, 2011. The Java 7 Summit offers Java developers and architects an opportunity to gain in-depth technical education on the new innovations introduced into the recent 
   <a href="http://www.oracle.com/us/corporate/events/java7/index.html">Java 7 release</a>.</p>
   
-  <p>Java 7 is the first major release of the platform in almost five years.  It introduces many improvements and new features including a new Fork/Join Framework, new File System API, 
-  enhanced support for Language Implementers and a collection of language changes managed in a project known as "Coin".  It also represents the first release where most of the development 
-  has been done in OpenJDK, with significant contributions coming from the Java community.  Attendees of the Summit will receive key technical education on these new Java 7 features, and 
+  <p>Java 7 is the first major release of the platform in almost five years. It introduces many improvements and new features including a new Fork/Join Framework, new File System API, 
+  enhanced support for Language Implementers, and a collection of language changes managed in a project known as "Coin." It also represents the first release where most of the development 
+  has been done in OpenJDK, with significant contributions coming from the Java community. Attendees of the Summit will receive key technical education on these new Java 7 features, and 
   learn more about OpenJDK.</p>
 
-   <p>The Summit will feature expert speakers from Oracle and the wider OpenJDK community.  Java experts interested in speaking at the Java 7 Summit are welcomed to propose a session through 
-   the <a href="http://www.eclipsecon.org/europe2011/content/submissions-are-open">Call for Papers of EclipseCon Europe</a>.   The deadline for proposals is August 17, 2011.</p>
+   <p>The Summit features expert speakers from Oracle and the wider OpenJDK community. Java experts interested in speaking at the Java 7 Summit are welcome to propose a session through 
+   the <a href="http://www.eclipsecon.org/europe2011/content/submissions-are-open">Call for Papers for EclipseCon Europe</a>. The deadline for proposals is August 17, 2011.</p>
 
-   <p>Registration for the Java 7 Summit will open in the next couple of weeks.  Java 7 Summit will be co-located with EclipseCon Europe so attendees will have access to all the sessions. </p>
+   <p>Registration for the Java 7 Summit will open in the next couple of weeks. The Java 7 Summit is co-located with EclipseCon Europe, and attendees will have access to all sessions. </p>
 
 	</div>
 
