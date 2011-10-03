8037adf144341e3945fc45bd9414421fc1bbdba2
wbeaton <wbeaton>
2011-10-03 15:48:24 +0000

    REOPENED - bug 356732: Create bio pages for Paul Clenahan and Eric Clayberg https://bugs.eclipse.org/bugs/show_bug.cgi?id=356732

---
+++ foundation/boardbios/Eric_Clayberg.php
@@ -0,0 +1,14 @@
+<img src="boardbios/photos/Eric_Clayberg.jpg" align="left" style="padding-right: 5px;" width="120">
+<p>Eric Clayberg is a Software Engineering Manager at Google and former
+co-founder and VP of Product Development for Instantiations (acquired
+by Google in 2010). He currently leads the Google Web Toolkit (GWT)
+and Google Plugin for Eclipse (GPE) projects and is the Project Lead
+for the WindowBuilder project at Eclipse.org
+(http://www.eclipse.org/windowbuilder).</p> 
+
+<p>He holds a BS degree in Aerospace Engineering from MIT, an MBA from
+Harvard, and is co-author of "Eclipse Plug-ins" (3rd Edition, Addison
+Wesley , Dec 2008), and "The Eclipse Graphical Editing Framework
+(GEF)" (Addison Wesley, Aug 2011). See http://www.qualityeclipse.com.</p>
+
+<p>In his spare time, he enjoys one of world's largest collections of classic coin-op video games.</p>
---
+++ foundation/boardbios/Paul_Clenahan.php
@@ -0,0 +1,13 @@
+<img src="boardbios/photos/Paul_Clenahan.jpg" align="left" style="padding-right: 5px;" width="120">
+<p>Paul Clenahan is a founding member of the Eclipse BIRT Project and
+a continuing member of the BIRT Project PMC (Project Management
+Committee). Paul has over 20 years experience in the enterprise
+business intelligence industry working in a variety of Product
+Management and consulting roles with diverse customer applications.</p>
+
+<p>In addition to his BIRT project role, Paul is VP of Product
+Management at Actuate and is responsible for coordinating overall
+product direction and vision for Actuate’s Business Intelligence
+technology based on emerging technology trends, market demands,
+customer feedback and internal innovation. Paul holds a B.Sc.
+in Computer Science from Edinburgh University, Scotland.</p>
