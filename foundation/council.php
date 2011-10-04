98bab880033ce5a3be9963256c5afc3ddca7a84b
wbeaton <wbeaton>
2011-10-04 14:07:43 +0000

    NEW - bug 359782: Remove "Requirements Council" from Councils page https://bugs.eclipse.org/bugs/show_bug.cgi?id=359782

--- foundation/council.php
+++ foundation/council.php
@@ -1,57 +1,32 @@
 <?php
+/*******************************************************************************
+* Copyright (c) 205, 2011 Eclipse Foundation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Denis Roy (Eclipse Foundation)- initial API and implementation
+*    Wayne Beaton (Eclipse Foundation) - Bug 359782
+*******************************************************************************/
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-require_once("/home/data/httpd/eclipse-php-classes/system/dbconnection_foundation_ro.class.php");
-#*****************************************************************************
-#
-# council.php
-#
-# Author: 		Denis Roy
-# Date:			2005-11-07
-#
-# Description:  Show the members of the requirements, planning, and
-#	architecture councils
-#
-# History:		Karl Matthias
-#				2007-04-30
-#				Lists now generated from the Foundation DB records
-#
-#
-#****************************************************************************
+$App  = new App();
+$Nav = new Nav();
+$Menu = new Menu();
+include($App->getProjectCommon());  
 
-#
-# Begin: page-specific settings.  Change these.
 $pageTitle 		= "Eclipse Foundation Councils";
 $pageKeywords	= "foundation, legal, councils, emo";
 $pageAuthor		= "Mike Milinkovich, Nov. 22, 2005";
 
-# Add page-specific Nav bars here
-# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-# $Nav->addCustomNav("My Link", "mypage.php", "_self", 1);
-# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 1);
-
-# End: page-specific settings
-#
-
-# DATABASE HOST
-$dbhost = 'localhost';
-
-$fdbc = new DBConnectionFoundation();
-$dbh = $fdbc->connect()
-	or die('Could not connect: ' . mysql_error());
-
-# Global Functions
-
-function mysql_error_check() {
-	if(mysql_errno() != 0) {
-		die('<h1 style="color: red">' . mysql_errno() . ': ' . mysql_error() . '</h1>');
-	}
-}
 
 # Lookup the relations and return the list
 function find_relations($relations_array, $include_year = false) {
-	global $dbh;
+	global $App;
 
 	$relations = "'" . implode( "','", $relations_array ) . "'";
 	/*
@@ -76,18 +51,18 @@ function find_relations($relations_array, $include_year = false) {
 			$rel = $r;
 		}
 	}
-	$result = mysql_query("SELECT PersonID
+	$result = $App->foundation_sql("SELECT PersonID
 		FROM PeopleRelations
 		WHERE Relation = '$rel'
 		");
-	mysql_error_check();
+	
 	while($row = mysql_fetch_assoc($result)) {
 		$chairs[$row['PersonID']] = 1;
 	}
 	/*
 	 * appointed members with company relationships
 	 */
-	$result = mysql_query("SELECT distinct(People.PersonID), FName, LName, Name1, year(EntryDate) as year
+	$result = $App->foundation_sql("SELECT distinct(People.PersonID), FName, LName, Name1, year(EntryDate) as year
 		FROM PeopleRelations, People, Organizations,
 			OrganizationContacts
 		WHERE PeopleRelations.Relation IN ($relations)
@@ -96,7 +71,7 @@ function find_relations($relations_array, $include_year = false) {
 				People.PersonID
 			AND Organizations.OrganizationID =
 				OrganizationContacts.OrganizationID");
-	mysql_error_check();
+
 	while($row = mysql_fetch_assoc($result)) {
 		$people[ucwords($row['LName'].', '.$row['FName'])] = array(
 		'<td>' .
@@ -113,12 +88,11 @@ function find_relations($relations_array, $include_year = false) {
 	/*
 	 * appointed members without company relationships
 	 */
-	$result = mysql_query("SELECT distinct(People.PersonID), FName, LName, year(EntryDate) as year
+	$result = $App->foundation_sql("SELECT distinct(People.PersonID), FName, LName, year(EntryDate) as year
 		FROM PeopleRelations, People
 		WHERE PeopleRelations.Relation IN ($relations)
 			AND People.PersonID = PeopleRelations.PersonID
 			");
-	mysql_error_check();
 	while($row = mysql_fetch_assoc($result)) {
 		if( !isset($people[ucwords($row['LName'].', '.$row['FName'])]) ) {
 			$people[ucwords($row['LName'].', '.$row['FName'])] = array(
@@ -136,7 +110,7 @@ function find_relations($relations_array, $include_year = false) {
 	/*
 	 * company members
 	 */
-	$result = mysql_query("SELECT distinct(People.PersonID),
+	$result = $App->foundation_sql("SELECT distinct(People.PersonID),
 			Name1, FName, LName
 		FROM OrganizationContacts, Organizations, People
 		WHERE OrganizationContacts.Relation IN ($relations)
@@ -144,7 +118,7 @@ function find_relations($relations_array, $include_year = false) {
 				Organizations.OrganizationID
 			AND OrganizationContacts.PersonID =
 				People.PersonID");
-	mysql_error_check();
+	
 	while($row = mysql_fetch_assoc($result)) {
 		$people[ucwords($row['LName'].', '.$row['FName'])] = array(
 		'<td>' .
@@ -158,7 +132,7 @@ function find_relations($relations_array, $include_year = false) {
 	/*
 	 * project members with company relationships
 	 */
-	$result = mysql_query("SELECT distinct(People.PersonID),
+	$result = $App->foundation_sql("SELECT distinct(People.PersonID),
 			ProjectID, FName, LName, Name1
 		FROM PeopleProjects, People, OrganizationContacts,
 			Organizations
@@ -172,7 +146,7 @@ function find_relations($relations_array, $include_year = false) {
 				'CC'
 			AND Organizations.OrganizationID =
 				OrganizationContacts.OrganizationID");
-	mysql_error_check();
+	
 	while($row = mysql_fetch_assoc($result)) {
 		$people[ucwords($row['LName'].', '.$row['FName'])] = array(
 		'<td>' .
@@ -187,7 +161,7 @@ function find_relations($relations_array, $include_year = false) {
 	/*
 	 * project members without company relationships
 	 */
-	$result = mysql_query("SELECT distinct(People.PersonID),
+	$result = $App->foundation_sql("SELECT distinct(People.PersonID),
 			ProjectID, FName, LName
 		FROM PeopleProjects, People
 		WHERE PeopleProjects.Relation IN ($relations)
@@ -195,7 +169,7 @@ function find_relations($relations_array, $include_year = false) {
 				People.PersonID
 			AND InactiveDate IS NULL
 			");
-	mysql_error_check();
+	
 	while($row = mysql_fetch_assoc($result)) {
 		if( !isset($people[ucwords($row['LName'].', '.$row['FName'])]) ) {
 			$people[ucwords($row['LName'].', '.$row['FName'])] = array(
@@ -217,96 +191,27 @@ ob_start();
 ?>
 <div id="midcolumn">
 	<h1><?= $pageTitle ?></h1>
-	<h2>Eclipse Council Structure</h2>
-	<p>As defined by the <a href="../documents/Eclipse%20Development%20Process%202003_11_09%20FINAL.pdf">Eclipse Development Process</a>,
-	the open source projects in Eclipse are guided and co-ordinated by three Councils:</p>
+	<p>As defined by the <a href="/projects/dev_process/development_process.php">Eclipse Development Process</a>,
+	the open source projects in Eclipse are guided and co-ordinated by two Councils:</p>
 	<ul class="midlist">
-        <li><strong><a href="#requirements">Requirements</a></strong>: The Requirements
-          Council is responsible for capturing and organizing requirements for
-          all of the projects in the Eclipse community. The Requirements Council
-          reviews and categorizes all of these incoming requirements - from all
-          residents of the Ecosystem - and proposes a coherent set of Themes and
-          Priorities that will drive the Roadmap.
-          <br/><br/>
-        </li>
         <li><strong><a href="#planning">Planning</a></strong>: The Planning Council
           is responsible for establishing a coordinated Platform Release Plan
           that supports the Roadmap, and balances the many competing requirements.
           The Platform Release Plan describes the themes and priorities that focus
           these Releases, and orchestrates the dependencies among Project Plans.
-          <br/><br/>
         </li>
         <li><strong><a href="#architecture">Architecture</a></strong>: The Architecture
-		  Council is responsible for the long-term technical health of the Eclipse
-		  platforms and frameworks. More explanation of the Architecture Council
+		  Council is responsible for (i) monitoring, guiding, and influencing the software 
+		  architectures used by Projects,  (ii) new  project mentoring,  and (iii) maintaining  
+		  and revising  the Eclipse Development Process subject to the approval of the Board. 
+		  More explanation of the Architecture Council
 		  can be found in the <a href="/projects/dev_process/development_process.php#4_5_Councils">Eclipse Development Process</a> and in
-		  the <a href="/projects/dev_process/architecture-council.php">guidelines and checklists</a>
+		  the <a href="http://wiki.eclipse.org/Architecture_Council">guidelines and checklists</a>
 		  for the Architecture Council.</li>
       </ul>
-      <br/>
-	<div class="homeitem3col">
-		<h3><a name="councildocuments"></a>Eclipse council documents</h3>
-		<blockquote>
-		<p> As described in the previous section, the <a href="../documents/Eclipse%20Development%20Process%202003_11_09%20FINAL.pdf">Eclipse
-        	Development Process</a> calls for the Eclipse Councils to create the Eclipse
-        	Roadmap documentation for approval by the Board of Directors. The <a href="/org/councils/roadmap.html">
-      		Eclipse Roadmap V2.0</a> has now been released.  If you are looking for Board or Members meeting minutes,
-      		<a href="minutes.php"> click here.</a>
-        </p>
-        </blockquote>
-        </div>
-	<div class="homeitem3col">
-	<h3><a name="requirements"></a>Requirements council</h3>
-	<br/>
-	<table width="95%" border="0" cellspacing="0" cellpadding="1" align="center">
-<?php
-// Requirements Council
-$requirements_relations = array( 'ER', 'RC', 'RZ' );
-$people = find_relations($requirements_relations);
-ksort($people);
-foreach($people as $name=>$value) {
-	echo "		<tr>" . $value[0] . "</tr>\n";
-}
-?>
-  	</table>
-	<br/>
-<ul>
 
-<li>Recent minutes are tracked on the <a href="http://wiki.eclipse.org/Requirements_Council">Requirements Council WIKI Page.</a>
-
-
- </li>
-
-<li><a href="../councils/20070122RCMinutes.pdf">Requirements Council Meeting,
-          		January 22, 2007 (.pdf)</a>
-    <ul>
-    <li><a href="../councils/20070122RC_Zend.pdf">Zend Member Presentation,
-          		January 22, 2007 (.pdf)</a>
-    <li><a href="../councils/20070122RC_Serena.pdf">Serena Member Presentation,
-          		January 22, 2007 (.pdf)</a>
-    </ul>
-
-    </li>
-
-<li><a href="../councils/20061010RCMinutes.pdf">Requirements Council Meeting,
-          		October 10, 2006 (.pdf)</a></li>
-
-	<li><a href="../councils/20060628RCMinutes.pdf">Requirements Council Meeting,
-          		June 28, 2006 (.pdf)</a></li>
-
-	 <li><a href="../councils/20060319RCMinutes.pdf">Requirements Council Meeting,
-          		March 19, 2006 (.pdf)</a></li>
-
- 	<li><a href="../councils/rcminutes050824.html">Requirements Council Meeting,
-          		August 24, 2005</a></li>
-        <li><a href="../councils/20041130RCMinutes.pdf">Requirements Council Meeting, November 30, 2004 (.pdf)</a></li>
-        <li><a href="../councils/20040831RCMinutes.pdf">Requirements Council Meeting,
-            August 31, 2004 (.pdf)</a></li>
-		</ul>
-	</div>
 <div class="homeitem3col">
 	<h3><a name="planning"></a>Planning council</h3>
-	<br/>
 
 	<table width="95%" border="0" cellspacing="0" cellpadding="1" align="center">
 <?php
@@ -335,8 +240,8 @@ $people = find_relations($architecture_relations, true);
 ksort($people);
 
 $mentors = array();
-$result = mysql_query("SELECT PP.PersonID, PP.ProjectID, P.UrlIndex FROM PeopleProjects PP INNER JOIN Projects P ON PP.ProjectId = P.ProjectID  WHERE Relation = 'ME' and InactiveDate is null");
-mysql_error_check();
+$result = $App->foundation_sql("SELECT PP.PersonID, PP.ProjectID, P.UrlIndex FROM PeopleProjects PP INNER JOIN Projects P ON PP.ProjectId = P.ProjectID  WHERE Relation = 'ME' and InactiveDate is null");
+
 while($obj = mysql_fetch_object($result)) {
 	if( !isset($mentors[$obj->PersonID]) ) $mentors[$obj->PersonID] = array();
 	if( $obj->UrlIndex != "" ) {
