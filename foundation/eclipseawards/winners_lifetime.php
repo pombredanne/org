df096d1a2560090cd15b2c6c4a0fa360bd1df780
cguindon <cguindon>
2011-11-29 03:09:07 +0000

    create lifetime awards

---
+++ foundation/eclipseawards/winners_lifetime.php
@@ -0,0 +1,67 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# sample_3col.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-11-07
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Eclipse Community Lifetime Awards Hall of Fame";
+	$pageKeywords	= "eclipse, foundation, community, award, winners, individual";
+	$pageAuthor		= "Christopher Guindon";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the markers!	
+ob_start();
+?>		
+<link rel="stylesheet" type="text/css" href="layout.css" media="screen" />
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+
+		<div class="homeitem3col">
+			<table>
+				<tr>
+					<td><img src="images/lifetime/david-williams-award.jpg" width="60"></td>
+					<td><b>David Williams</b><br>Lifetime Achievement Award 2011</td>
+					
+				</tr>
+				
+			</table>
+				
+		</div>	
+	</div>
+	
+	<!-- remove the entire <div> tag to omit the right column!  -->
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Related Links</h6>
+			<ul>
+				<li><a href="index.php">Eclipse Community Awards</a></li>
+				<li><a href="pastwinners.php">Past Winners</a></li>
+			</ul>
+		</div>
+	</div>
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage("Nova", $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
