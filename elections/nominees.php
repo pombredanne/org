d81ef9e6bf7dba79eecaee69f0565cb089b60a16
ajacko <ajacko>
2011-12-14 22:00:07 +0000

    *** empty log message ***

--- elections/nominees.php
+++ elections/nominees.php
@@ -45,7 +45,7 @@ function ignoreDumbStuffHandler($errno, $errmsg, $filename, $linenum, $vars) {
 	
 	include("scripts/candidate.php");	
 	//$id = $_GET['id'];
-	$year="2011";
+	$year="2012";
 	$candidates = get_all_candidates($year);
 	
 	#
