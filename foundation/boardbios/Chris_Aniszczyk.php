6121421287f74f32e93b9b6595472b39e7f79ca7
andross <andross>
2012-01-17 17:58:43 +0000

    Update Chris' bio

--- foundation/boardbios/Chris_Aniszczyk.php
+++ foundation/boardbios/Chris_Aniszczyk.php
@@ -1,9 +1,11 @@
 <img src="boardbios/photos/Chris_Aniszczyk.jpg" align="left" style="padding-right: 5px;" width="120">
-Chris Aniszczyk is the technical lead for the Eclipse Plug-in
-Development Environment (PDE) project and a Principal Software
-Engineer at Red Hat. He tends to be all over the place inside the
-Eclipse community by committing to and mentoring various Eclipse
-projects. He sits on the Eclipse Architecture Council and on the
-Eclipse Technology PMC. His passions are blogging, software advocacy,
-tooling and anything Eclipse. He's always available to discuss open
-source or Eclipse over a frosty beverage.
+Chris is a software architect by trade with a passion for software
+evangelism, open source, and building communities. At Twitter, he's 
+responsible for creating their open source program office and 
+managing their open source efforts. He sits on the Eclipse Architecture 
+Council and on the Eclipse Technology PMC. In a previous life, he led 
+and hacked on many <a href="http://eclipse.org">eclipse.org</a> and 
+linux related projects. In his spare time, you'll find him doing yoga,
+writing, running or cycling. You can read his 
+<a href="http://aniszczyk.org/">blog</a> or find him tweeting 
+via <a href="https://twitter.com/cra">@cra</a>.
