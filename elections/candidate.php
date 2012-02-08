64fefcdf99ba80b108d6a71258d5f968aa65dc84
ajacko <ajacko>
2012-02-08 03:23:30 +0000

    *** empty log message ***

--- elections/candidate.php
+++ elections/candidate.php
@@ -14,7 +14,7 @@
 	
 	include("scripts/candidate.php");	
 	$id = $_GET['id'];
-	$year = "2011";
+	$year = "2012";
 	$candidate = get_candidate($id, $year);
 	$candidates_summary = get_candidates_short_summary_as_html($year);
 	
