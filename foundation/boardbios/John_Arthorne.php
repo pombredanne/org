92788b6e11a3740e3d179887de27d82689536a5f
andross <andross>
2012-04-19 13:18:08 +0000

    Add bio for John Arthorne. Closes Bug 377181.

---
+++ foundation/boardbios/John_Arthorne.php
@@ -0,0 +1,10 @@
+<img src="boardbios/photos/John_Arthorne.jpg" align="left" style="padding-right: 5px;" width="120">
+John has worked on the <a href="http://wiki.eclipse.org/Eclipse">Eclipse</a> and <a href="http://wiki.eclipse.org/Equinox">Equinox</a> projects for the past
+decade in many different areas. He was the main developer on the
+resource model for many years, and designed the platform's 
+concurrency infrastructure. In recent years he has focused on
+the <a href="http://wiki.eclipse.org/Orion">Orion project</a>, provisioning (<a href="http://wiki.eclipse.org/P2">p2</a>), <a href="http://wiki.eclipse.org/E4">e4</a>, and overall platform
+<a href="http://wiki.eclipse.org/API_Central">API quality</a>. John is a member of the <a href="http://wiki.eclipse.org/Architecture_Council">Eclipse Architecture Council</a>,
+<a href="http://wiki.eclipse.org/Planning_Council">Planning Council</a>, <a href="http://wiki.eclipse.org/Eclipse/PMC">Eclipse Project PMC</a>, and the <a href="http://www.eclipse.org/org/foundation/directors.php">Eclipse Foundation
+Board of Directors</a>. John is a Senior Software Developer at IBM,
+based in Ottawa, Canada.
