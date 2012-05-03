b473b250b29086503219eb78cb585e2c2bcd0221
andross <andross>
2012-05-03 13:24:09 +0000

    Add bio and picture for Jutta Bindewald closes Bug 378368

---
+++ foundation/boardbios/Jutta_Bindewald.php
@@ -0,0 +1,6 @@
+<img src="boardbios/photos/Jutta_Bindewald.jpg" align="left" style="padding-right: 5px;" width="120">
+Jutta joined SAP in 1993 as a software developer. She has worked in various areas of SAP’s technology
+ platform, ranging from R/2 over R/3 to NetWeaver Java. Currently Jutta is working as Development 
+ Manager, leading the Java Persistence team. Additionally, she takes care of many Open Source related 
+ topics like Eclipse Long-Term Support. Jutta holds a Ph.D. in Mathematics from the Technical 
+ University of Darmstadt.
