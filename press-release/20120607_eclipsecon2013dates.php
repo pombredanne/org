4a1d536f6e1012427bdcb6a0c11a312966372051
iskerrett <iskerrett>
2012-06-07 13:59:18 +0000

    *** empty log message ***

---
+++ press-release/20120607_eclipsecon2013dates.php
@@ -0,0 +1,61 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include("_projectCommon.php");    # All on the same line to unclutter the user's desktop'
+
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "EclipseCon 2013 Coming to Boston";
+	$pageKeywords	= "eclipsecon eclipse java conference";
+	$pageAuthor		= "Ian Skerrett";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank)
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self");
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank");
+
+	# End: page-specific settings
+	#
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+	<style>
+		.paddedlist li {	padding-bottom:7px;	}
+		#midcolumn ul ul{padding-bottom:0px;}
+	</style>
+	
+<div id="maincontent">
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+<br>
+<p>The Eclipse Foundation is pleased to announce the  EclipseCon North American 2013 conference will be held March 25-28, 2013 in Boston, MA.  The conference will be
+located at the Seaport Hotel and World Trade Center.  More details about EclipseCon 2013 and the call for speakers will be made available in 
+
+<p>The next EclipseCon Europe is scheduled for October 23-25 in Ludwigsburg, Germany.</p>
+	</div>
+
+	<!-- remove the entire <div> tag to omit the right column!  -->
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Related Links</h6>
+			<ul>
+				<li><a href="http://eclipse.org/paho/">Eclipse Paho project</a></li>
+				<li><a href="http://eclipse.org/koneki/">Eclipse  Koneki project</a></li>
+				<li><a href="http://wiki.eclipse.org/Machine-to-Machine">Information about the M2M IWG</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
+<?php
+/*
+ * Created on 20-Jan-2006
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
