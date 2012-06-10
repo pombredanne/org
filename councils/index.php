7a407e5542e5ad5f3ebc19a7399c75a698cd9329
mmilinkovich <mmilinkovich>
2012-06-10 18:15:10 +0000

    Fixed the re-direct in org/councils/index/php to point to the current version of the Roadmap.

--- councils/index.php
+++ councils/index.php
@@ -3,6 +3,6 @@
       <meta http-equiv="refresh" content="0; url=roadmap_v4_0/index.php" />
    </head>
    <body>
-      <a href="roadmap_v4_0/index.php">The current roadmap is version 4.0.</a>
+      <a href="roadmap_v6_0/index.php">The current roadmap is version 6.0, published in 2011.</a>
    </body>
 </html>
