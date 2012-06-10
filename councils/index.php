09d0d8f2adb28efc2c1469e2ee0f9d5d247effba
mmilinkovich <mmilinkovich>
2012-06-10 18:18:50 +0000

    Fixed the link for the re-direct

--- councils/index.php
+++ councils/index.php
@@ -1,6 +1,6 @@
 <html>
    <head>
-      <meta http-equiv="refresh" content="0; url=roadmap_v4_0/index.php" />
+      <meta http-equiv="refresh" content="0; url=roadmap_v6_0/index.php" />
    </head>
    <body>
       <a href="roadmap_v6_0/index.php">The current roadmap is version 6.0, published in 2011.</a>
