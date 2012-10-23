031949e6eeb9a99b5035c8185aae09195711b1ed
Christopher Guindon <chris.guindon@eclipse.org>
2012-10-23 10:40:04 -0400

    initial commit

---
+++ press-release/20120923_eclipse_developers_survey_2012.php
@@ -0,0 +1,59 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include("_projectCommon.php");    # All on the same line to unclutter the user's desktop'
+
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Eclipse Developers Survey 2012";
+	$pageKeywords	= "eclipse survey, developers survey";
+	$pageAuthor		= "Christopher Guindon";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank)
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self");
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank");
+
+	# End: page-specific settings
+	#
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	
+<div id="maincontent">
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+<br>
+<p>The Eclipse Foundation would like to better understand how developers are using Eclipse with big data and reporting projects. Please take a few minutes to answer this <a href="http://www.surveymonkey.com/s/eclipse-data" target="_blank">four question survey</a>.</p>
+
+<p>Survey will end on November 23, 2012.</p>
+
+
+	</div>
+
+	<!-- remove the entire <div> tag to omit the right column!  -->
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Related Links</h6>
+			<ul>
+				<li><a href="http://www.surveymonkey.com/s/eclipse-data" target="_blank">four question survey</a></li>
+			
+			</ul>
+		</div>
+	</div>
+</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
+<?php
+/*
+ * Created on 20-Jan-2006
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+
