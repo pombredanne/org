6fcd7dbc8df7c4d6f395486d9b5e0e6c46950edb
Mike Milinkovich <mike.milinkovich@eclipse.org>
2012-11-23 16:00:51 -0500

    Posted the minutes of the April 18, 2012 Board meeting.

--- foundation/minutes.php
+++ foundation/minutes.php
@@ -42,6 +42,9 @@
 		<div class="homeitem3col">
 			<h3><a name="board">Board of Directors Meetings</a></h3>
 			<ul><li>Abridged Minutes 
+          			- <a href="boardminutes/2012_04_18_Minutes.php">April 18, 
+          			2012 (html)</a><img src="../../images/new.gif" alt="new"> </li>
+          		<li>Abridged Minutes 
           			- <a href="boardminutes/2012_03_26_Minutes.php">March 26, 
           			2012 (html)</a><img src="../../images/new.gif" alt="new"> </li>
           		<li>Abridged Minutes 
