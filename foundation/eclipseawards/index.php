95f66434b5c79ffaf32be95ab38146380ced0462
rjoncas <rjoncas@192.168.1.142>
2012-12-03 15:40:35 -0500

    Removed <div> for previous button

--- foundation/eclipseawards/index.php
+++ foundation/eclipseawards/index.php
@@ -67,7 +67,6 @@
 		
 		<h2>Project Nominations</h2>
 		<b><a class="button" href="http://marketplace.eclipse.org/node/add/project-nominations">Nominate</a></b>
-		</div>
 				<ul class="midlist">
 			<li><b>Most Innovative New Feature or Project</b> - A feature added in 2012 to an existing project hosted by the Eclipse Foundation or a new Eclipse project introduced in 2012 that is used and loved by the community.
 			<br>
@@ -80,7 +79,6 @@
 		
 		<h2>Technology Nominations</h2>
 		<b><a class="button" href="http://marketplace.eclipse.org/node/add/product-nominations">Nominate</a></b>
-		</div>
 		<ul class="midlist">
 			<li><b>Best Application</b> - An application that illustrates the most innovative use of Eclipse technology.
 			<br><a href="http://marketplace.eclipse.org/nominations/best-application" target="blank">Current nominees</a>	
