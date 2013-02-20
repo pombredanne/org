270edd9db0af31d1a5c0088d868528fa5bc3a9ca
Andrew Ross <fosslc@gmail.com>
2013-02-20 13:55:23 -0500

    add bio and picture for Pat Huff (IBM rep.)

---
+++ foundation/boardbios/Pat_Huff.php
@@ -0,0 +1,4 @@
+<img src="boardbios/photos/Pat_Huff.jpg" align="left" style="padding-right: 5px;" width="120">
+
+Pat Huff is a Program Director in IBM Rational, responsible for Open Source and Eclipse. Pat manages an Open Source Center of Competency and the relationship between the IBM product development community and  Eclipse. Pat has a long history of producing software development tools and frameworks, and in introducing open source and agile programming concepts into corporate infrastructures. His current interests lie in realizing Open Source's potential in commercially successful offerings. In addition to his board duties, Pat is the Chair of the Eclipse IP Advisory Committee and the co-chair of the Eclipse Long term Support Industry Working Group.
+
