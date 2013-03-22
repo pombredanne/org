9e38c8d7270e5b4f67056a7403029d980d0de950
Christopher Guindon <chris.guindon@eclipse.org>
2013-03-22 11:33:31 -0400

    [replace] IWG > WG [update] website links to WG.

--- industry-workgroups/index.php
+++ industry-workgroups/index.php
@@ -1,7 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-$pageTitle 		= "Eclipse Industry Working Groups";
+$pageTitle 		= "Eclipse Working Groups";
 $pageAuthor		= "";
-$pageKeywords	= "eclipse, industry, working, groups";
+$pageKeywords	= "eclipse, working, groups";
 
 ob_start();
 ?>
@@ -20,13 +20,13 @@ ob_start();
 	<div id="midcolumn">
 	<h1><?=$pageTitle;?></h1>
 		<div class="homeitem3col">
-			<p>Eclipse Industry Working Groups (IWGs) are established to facilitate collaboration between Eclipse Foundation Members. 
+			<p>Eclipse Working Groups (WGs) are established to facilitate collaboration between Eclipse Foundation Members. 
 				The collaboration is intended to focus, promote and augment Eclipse technologies to meet 
 				the needs of specific industries. The collaboration can take the form of the development of 
 				materials for use by a specific community and/or joint marketing programs to promote Eclipse 
 				in a certain industry. Unlike Eclipse open source projects that are open to participation by 
-				anyone, participation in an Eclipse IWG is open only to Eclipse Members.</p>
-			<h3>Current IWGs</h3>
+				anyone, participation in an Eclipse WG is open only to Eclipse Members.</p>
+			<h3>Current WGs</h3>
 			<div style="margin: 5px 15px 15px 15px; ">
 				<table cellspacing=0 width="100%">
 					<tr>
@@ -45,21 +45,21 @@ ob_start();
 					</tr>				
 					<tr>
 						<td>&nbsp;&nbsp;&nbsp;&nbsp;LocationTech</td>
-						<td><a href="http://wiki.eclipse.org/Location"><img src="/home/categories/images/homepage.gif"></a></td>
+						<td><a href="https://www.locationtech.org/"><img src="/home/categories/images/homepage.gif"></a></td>
 						<td><a href="locationtech_charter.php"><img src="/home/categories/images/html.png"></td>
-						<td><a href="https://dev.eclipse.org/mailman/listinfo/location-iwg"><img src="/home/categories/images/mailinglist.gif"></a></td>
+						<td><a href="https://www.locationtech.org/mailman/listinfo/location-iwg"><img src="/home/categories/images/mailinglist.gif"></a></td>
 						<td><a href="http://wiki.eclipse.org/Location"><img src="/home/categories/images/wiki.gif"></a></td>
 					</tr>										
 					<tr>
 						<td>&nbsp;&nbsp;&nbsp;&nbsp;Long-Term Support</td>
-						<td><a href="http://wiki.eclipse.org/LTS"><img src="/home/categories/images/homepage.gif"></a></td>
+						<td><a href="http://lts.eclipse.org/"><img src="/home/categories/images/homepage.gif"></a></td>
 						<td><a href="ltsiwg_charter.php"><img src="/home/categories/images/html.png"></a></td>
 						<td><a href="https://dev.eclipse.org/mailman/listinfo/lts-iwg"><img src="/home/categories/images/mailinglist.gif"></a></td>
 						<td><a href="http://wiki.eclipse.org/LTS"><img src="/home/categories/images/wiki.gif"></a></td>
 					</tr>						
 					<tr>
 						<td>&nbsp;&nbsp;&nbsp;&nbsp;Machine-to-Machine</td>
-						<td><a href="http://wiki.eclipse.org/Machine-to-Machine"><img src="/home/categories/images/homepage.gif"></a></td>
+						<td><a href="http://m2m.eclipse.org/"><img src="/home/categories/images/homepage.gif"></a></td>
 						<td><a href="m2miwg_charter.php"><img src="/home/categories/images/html.png"></a></td>
 						<td><a href="https://dev.eclipse.org/mailman/listinfo/m2m-iwg"><img src="/home/categories/images/mailinglist.gif"></a></td>
 						<td><a href="http://wiki.eclipse.org/Machine-to-Machine"><img src="/home/categories/images/wiki.gif"></a></td>
@@ -74,7 +74,7 @@ ob_start();
 				</table>
 			</div>
 
-			<h3>Inactive IWGs</h3>
+			<h3>Inactive WGs</h3>
 				<div style="margin: 5px 15px 15px 15px; ">
 				<table cellspacing=0 width="100%">
 					<tr>
